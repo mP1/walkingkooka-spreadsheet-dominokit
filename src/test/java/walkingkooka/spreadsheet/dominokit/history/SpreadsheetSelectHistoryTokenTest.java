6354ee61d06ca29dba1c947107bc42f63c65ef17
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-03-19 21:08:19 +1100

    SpreadsheetSelectHistoryTokenTest extends SpreadsheetNameHistoryTokenTestCase fix

--- src/test/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetSelectHistoryTokenTest.java
+++ src/test/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetSelectHistoryTokenTest.java
@@ -19,7 +19,7 @@ package walkingkooka.spreadsheet.dominokit.history;
 
 import org.junit.jupiter.api.Test;
 
-public final class SpreadsheetSelectHistoryTokenTest extends SpreadsheetIdHistoryTokenTestCase<SpreadsheetSelectHistoryToken> {
+public final class SpreadsheetSelectHistoryTokenTest extends SpreadsheetNameHistoryTokenTestCase<SpreadsheetSelectHistoryToken> {
 
     @Test
     public void testUrlFragment() {
