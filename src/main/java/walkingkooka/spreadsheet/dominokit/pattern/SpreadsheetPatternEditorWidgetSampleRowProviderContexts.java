47411b59689c52b820015965559c4a010bcc814d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-07-09 19:57:26 +1000

    SpreadsheetPatternEditorWidgetSampleRowProviderContexts.basic was with fix

--- src/main/java/walkingkooka/spreadsheet/dominokit/pattern/SpreadsheetPatternEditorWidgetSampleRowProviderContexts.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/pattern/SpreadsheetPatternEditorWidgetSampleRowProviderContexts.java
@@ -24,23 +24,23 @@ import walkingkooka.spreadsheet.format.pattern.SpreadsheetPatternKind;
 public final class SpreadsheetPatternEditorWidgetSampleRowProviderContexts implements PublicStaticHelper {
 
     /**
-     * {@see SpreadsheetPatternEditorWidgetSampleRowProviderContext}
-     */
-    public static SpreadsheetPatternEditorWidgetSampleRowProviderContext fake() {
-        return new FakeSpreadsheetPatternEditorWidgetSampleRowProviderContext();
-    }
-
-    /**
      * {@see BasicSpreadsheetPatternEditorWidgetSampleRowProviderContext}
      */
-    public static SpreadsheetPatternEditorWidgetSampleRowProviderContext with(final SpreadsheetPatternKind kind,
-                                                                              final String patternText,
-                                                                              final SpreadsheetFormatterContext spreadsheetFormatterContext) {
+    public static SpreadsheetPatternEditorWidgetSampleRowProviderContext basic(final SpreadsheetPatternKind kind,
+                                                                               final String patternText,
+                                                                               final SpreadsheetFormatterContext spreadsheetFormatterContext) {
         return BasicSpreadsheetPatternEditorWidgetSampleRowProviderContext.with(
                 kind,
                 patternText,
                 spreadsheetFormatterContext
         );
+    }
+
+    /**
+     * {@see SpreadsheetPatternEditorWidgetSampleRowProviderContext}
+     */
+    public static SpreadsheetPatternEditorWidgetSampleRowProviderContext fake() {
+        return new FakeSpreadsheetPatternEditorWidgetSampleRowProviderContext();
     }
 
     /**
