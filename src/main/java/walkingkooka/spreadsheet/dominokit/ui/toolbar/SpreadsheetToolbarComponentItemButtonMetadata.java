57c7592d7cb30cd44a287c25f3bdc54de224cacf
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-01-27 14:15:50 +1100

    SpreadsheetToolbarComponentItemButtonMetadataHideZeroValues

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/toolbar/SpreadsheetToolbarComponentItemButtonMetadata.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.ui.toolbar;
+
+import elemental2.dom.HTMLElement;
+import org.dominokit.domino.ui.icons.MdiIcon;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.ui.NopRefreshComponentLifecycle;
+import walkingkooka.spreadsheet.dominokit.ui.SpreadsheetCellComponentLifecycle;
+import walkingkooka.spreadsheet.dominokit.ui.VisibleComponentLifecycle;
+
+/**
+ * Base class for buttons that update a {@link walkingkooka.spreadsheet.meta.SpreadsheetMetadata} property.
+ */
+abstract class SpreadsheetToolbarComponentItemButtonMetadata extends SpreadsheetToolbarComponentItemButton
+        implements SpreadsheetCellComponentLifecycle,
+        NopRefreshComponentLifecycle,
+        VisibleComponentLifecycle<HTMLElement> {
+
+    SpreadsheetToolbarComponentItemButtonMetadata(final String id,
+                                                  final MdiIcon icon,
+                                                  final String tooltipText,
+                                                  final AppContext context) {
+        super(
+                id,
+                icon,
+                tooltipText,
+                context
+        );
+        this.context = context;
+    }
+
+    @Override //
+    final void onToolbarRefreshBegin() {
+
+    }
+
+    @Override //
+    final void onToolbarRefreshSelectedCell(final SpreadsheetCell cell,
+                                            final AppContext context) {
+
+    }
+
+    @Override //
+    final void onToolbarRefreshEnd(final int cellPresentCount,
+                                   final AppContext context) {
+        this.refreshButton(context);
+    }
+
+    abstract void refreshButton(final AppContext context);
+
+    /**
+     * {@link walkingkooka.spreadsheet.meta.SpreadsheetMetadata} properties are always shown.
+     */
+    @Override
+    public final boolean isMatch(final HistoryToken token) {
+        return true; // always show
+    }
+
+    /**
+     * Must be {@link AppContext} so sub-classes can get current {@link walkingkooka.spreadsheet.meta.SpreadsheetMetadata}.
+     */
+    final AppContext context;
+}
---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/toolbar/SpreadsheetToolbarComponentItemButtonMetadataHideZeroValues.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.ui.toolbar;
+
+import elemental2.dom.Event;
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.SpreadsheetMetadataPropertyHistoryToken;
+import walkingkooka.spreadsheet.dominokit.ui.SpreadsheetIcons;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+
+import java.util.Objects;
+
+/**
+ * When clicked, updates the {@link SpreadsheetMetadataPropertyName#HIDE_ZERO_VALUES} with the opposite of its current value.
+ */
+final class SpreadsheetToolbarComponentItemButtonMetadataHideZeroValues extends SpreadsheetToolbarComponentItemButtonMetadata {
+
+    static SpreadsheetToolbarComponentItemButtonMetadataHideZeroValues with(final AppContext context) {
+        Objects.requireNonNull(context, "context");
+
+        return new SpreadsheetToolbarComponentItemButtonMetadataHideZeroValues(
+                context
+        );
+    }
+
+    private SpreadsheetToolbarComponentItemButtonMetadataHideZeroValues(final AppContext context) {
+        super(
+                SpreadsheetToolbarComponent.hideZeroValues(),
+                SpreadsheetIcons.hideZeroValues(),
+                "",
+                context
+        );
+    }
+
+    @Override
+    void onClick(final Event event) {
+        final AppContext context = this.context;
+
+        final HistoryToken historyToken = context.historyToken();
+        final boolean hide = this.isHideZeroValues(context);
+
+        context.pushHistoryToken(
+                historyToken.setMetadataPropertyName(SpreadsheetMetadataPropertyName.HIDE_ZERO_VALUES)
+                        .setSave(
+                                false == hide // if hide=true then click makes hide=false
+                        )
+        );
+    }
+
+    @Override
+    void onFocus(final Event event) {
+        // TODO need to decide on history token
+    }
+
+    @Override
+    void refreshButton(final AppContext context) {
+        final boolean hide = this.isHideZeroValues(context);
+
+        this.setButtonSelected(
+                hide,
+                context
+        );
+
+        this.setTooltipText(
+                hide ?
+                        "Show Zero Values" :
+                        "Hide Zero Values"
+        );
+    }
+
+    private boolean isHideZeroValues(final AppContext context) {
+        return context.spreadsheetMetadata()
+                .getOrFail(SpreadsheetMetadataPropertyName.HIDE_ZERO_VALUES);
+    }
+
+    // ComponentLifecycle...............................................................................................
+
+    @Override
+    public boolean shouldIgnore(final HistoryToken token) {
+        boolean ignore = false;
+
+        if (token instanceof SpreadsheetMetadataPropertyHistoryToken) {
+            final SpreadsheetMetadataPropertyHistoryToken metadata = token.cast(SpreadsheetMetadataPropertyHistoryToken.class);
+            ignore = false == SpreadsheetMetadataPropertyName.HIDE_ZERO_VALUES.equals(metadata.propertyName());
+        }
+
+        return ignore;
+    }
+}
