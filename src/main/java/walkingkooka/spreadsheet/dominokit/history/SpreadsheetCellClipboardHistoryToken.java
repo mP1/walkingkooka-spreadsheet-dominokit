1cb5c733af339e304c461ea3a0a7ed17dc324074
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-03-11 20:38:57 +1100

    SpreadsheetCellClipboardValueKind clipboard package was history

--- src/main/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetCellClipboardHistoryToken.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetCellClipboardHistoryToken.java
@@ -20,6 +20,7 @@ package walkingkooka.spreadsheet.dominokit.history;
 import walkingkooka.net.UrlFragment;
 import walkingkooka.spreadsheet.SpreadsheetId;
 import walkingkooka.spreadsheet.SpreadsheetName;
+import walkingkooka.spreadsheet.dominokit.clipboard.SpreadsheetCellClipboardValueKind;
 import walkingkooka.spreadsheet.format.pattern.SpreadsheetPatternKind;
 import walkingkooka.spreadsheet.reference.AnchoredSpreadsheetSelection;
 
--- src/main/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetSelectionHistoryToken.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetSelectionHistoryToken.java
@@ -20,6 +20,7 @@ package walkingkooka.spreadsheet.dominokit.history;
 import walkingkooka.net.UrlFragment;
 import walkingkooka.spreadsheet.SpreadsheetId;
 import walkingkooka.spreadsheet.SpreadsheetName;
+import walkingkooka.spreadsheet.dominokit.clipboard.SpreadsheetCellClipboardValueKind;
 import walkingkooka.spreadsheet.dominokit.ui.SpreadsheetCellFind;
 import walkingkooka.spreadsheet.reference.SpreadsheetCellRangePath;
 import walkingkooka.text.cursor.TextCursor;
