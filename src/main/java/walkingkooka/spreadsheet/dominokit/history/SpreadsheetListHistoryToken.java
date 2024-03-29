593957996a33b61a788e81a3694ce9d5fe17a98a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-03-29 14:39:14 +1100

    SpreadsheetListHistoryToken
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/2150
    - SpreadsheetListHistoryToken

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetListHistoryToken.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.history;
+
+import walkingkooka.net.UrlFragment;
+import walkingkooka.spreadsheet.SpreadsheetId;
+import walkingkooka.spreadsheet.SpreadsheetName;
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.text.cursor.TextCursor;
+
+/**
+ * A token that represents a spreadsheet list files dialog.
+ */
+public final class SpreadsheetListHistoryToken extends SpreadsheetHistoryToken {
+
+    static SpreadsheetListHistoryToken with() {
+        return new SpreadsheetListHistoryToken();
+    }
+
+    private SpreadsheetListHistoryToken() {
+        super();
+    }
+
+    @Override
+    public UrlFragment urlFragment() {
+        return UrlFragment.SLASH;
+    }
+
+    @Override
+    HistoryToken parse0(final String component,
+                        final TextCursor cursor) {
+        return this;
+    }
+
+    @Override
+    public HistoryToken clearAction() {
+        return this;
+    }
+
+    @Override
+    public HistoryToken setFormula() {
+        return this; // should not happen
+    }
+
+    @Override //
+    HistoryToken replaceIdAndName(final SpreadsheetId id,
+                                  final SpreadsheetName name) {
+        return spreadsheetSelect(
+                id,
+                name
+        );
+    }
+
+    @Override
+    public void onHistoryTokenChange(final HistoryToken previous,
+                                     final AppContext context) {
+        // nop
+    }
+}
