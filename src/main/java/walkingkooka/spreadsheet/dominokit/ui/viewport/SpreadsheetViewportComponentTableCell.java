9d5c1e5ff4bbc080e589900ae71f819196a24f91
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-07 01:23:43 +1100

    SpreadsheetViewportComponentTableCell commented code removed

--- src/main/java/walkingkooka/spreadsheet/dominokit/ui/viewport/SpreadsheetViewportComponentTableCell.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/viewport/SpreadsheetViewportComponentTableCell.java
@@ -42,59 +42,9 @@ abstract class SpreadsheetViewportComponentTableCell {
         super();
     }
 
-//    private TextStyle columnHeaderStyle(final SpreadsheetColumnReference column) {
-//        return this.columnRowHeaderStyle(column);
-//    }
-//
-//    private TextStyle rowHeaderStyle(final SpreadsheetRowReference row) {
-//        return this.columnRowHeaderStyle(row);
-//    }
-
-//    private TextStyle columnRowHeaderStyle(final SpreadsheetSelection selection) {
-//        return this.isSelected(selection) ?
-//                COLUMN_ROW_HEADER_SELECTED_STYLE :
-//                COLUMN_ROW_HEADER_UNSELECTED_STYLE;
-//    }
-
-//    final static TextStyle COLUMN_ROW_HEADER_SELECTED_STYLE;
-//    final static TextStyle COLUMN_ROW_HEADER_UNSELECTED_STYLE;
-
     final static Color BORDER_COLOR = Color.BLACK;
     final static BorderStyle BORDER_STYLE = BorderStyle.SOLID;
     final static Length<?> BORDER_LENGTH = Length.pixel(1.0);
-
-//    static {
-//        final TextStyle style = TextStyle.EMPTY
-//                .set(
-//                        TextStylePropertyName.MARGIN,
-//                        Length.none()
-//                ).setBorder(
-//                        BORDER_COLOR,
-//                        BORDER_STYLE,
-//                        BORDER_LENGTH
-//                ).set(
-//                        TextStylePropertyName.PADDING,
-//                        Length.none()
-//                ).setValues(
-//                        Maps.of(
-//                                TextStylePropertyName.TEXT_ALIGN,
-//                                TextAlign.CENTER,
-//                                TextStylePropertyName.VERTICAL_ALIGN,
-//                                VerticalAlign.MIDDLE,
-//                                TextStylePropertyName.FONT_WEIGHT,
-//                                FontWeight.NORMAL
-//                        )
-//                );
-////        COLUMN_ROW_HEADER_SELECTED_STYLE = style.set(
-////                TextStylePropertyName.BACKGROUND_COLOR,
-////                SpreadsheetDominoKitColor.VIEWPORT_HEADER_SELECTED_BACKGROUND_COLOR
-////        );
-////        COLUMN_ROW_HEADER_UNSELECTED_STYLE = style.set(
-////                TextStylePropertyName.BACKGROUND_COLOR,
-////                SpreadsheetDominoKitColor.VIEWPORT_HEADER_UNSELECTED_BACKGROUND_COLOR
-////        );
-//    }
-
 
     static String css(final TextStyle style,
                       final Length<?> width,
@@ -113,22 +63,6 @@ abstract class SpreadsheetViewportComponentTableCell {
         ).css() + "box-sizing: border-box;";
     }
 
-//    static String css(final Length<?> width,
-//                      final Length<?> height) {
-//        return STYLE.setValues(
-//                Maps.of(
-//                        TextStylePropertyName.MIN_WIDTH,
-//                        width,
-//                        TextStylePropertyName.WIDTH,
-//                        width,
-//                        TextStylePropertyName.MIN_HEIGHT,
-//                        height,
-//                        TextStylePropertyName.HEIGHT,
-//                        height
-//                )
-//        ).css() + "box-sizing: border-box;";
-//    }
-
     private final static TextStyle STYLE = TextStyle.EMPTY
             .set(
                     TextStylePropertyName.MARGIN,
@@ -141,12 +75,6 @@ abstract class SpreadsheetViewportComponentTableCell {
             ).set(
                     TextStylePropertyName.PADDING,
                     Length.none()
-//            ).set(
-//                    TextStylePropertyName.VERTICAL_ALIGN,
-//                    VerticalAlign.TOP
-//            ).set(
-//                    TextStylePropertyName.FONT_FAMILY,
-//                    FontFamily.with("MS Sans Serif")
             ).set(
                     TextStylePropertyName.FONT_SIZE,
                     FontSize.with(11)
@@ -166,7 +94,6 @@ abstract class SpreadsheetViewportComponentTableCell {
                     TextStylePropertyName.WORD_BREAK,
                     WordBreak.NORMAL
             );
-
 
     final static TextStyle CELL_STYLE = STYLE
             .set(
