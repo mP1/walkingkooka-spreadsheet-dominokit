0feb39af6c9177c89301465f3a99212a38965270
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-11 11:17:45 +1000

    SpreadsheetDialogComponent height fits content

--- src/main/java/walkingkooka/spreadsheet/dominokit/ui/dialog/SpreadsheetDialogComponent.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/dialog/SpreadsheetDialogComponent.java
@@ -17,6 +17,7 @@
 
 package walkingkooka.spreadsheet.dominokit.ui.dialog;
 
+import elemental2.dom.CSSStyleDeclaration;
 import elemental2.dom.HTMLDivElement;
 import org.dominokit.domino.ui.IsElement;
 import org.dominokit.domino.ui.dialogs.Dialog;
@@ -86,6 +87,16 @@ public class SpreadsheetDialogComponent implements HtmlElementComponent<HTMLDivE
                 .id(id);
 
         this.setTitle(title);
+
+        // Stops extra empty area below some dialogs
+        final CSSStyleDeclaration style = this.element()
+                .style;
+
+        style.setProperty("margin-top", "20vh"); // not sure why 10vh gives no space
+        style.setProperty("margin-bottom", "10vh");
+
+        style.setProperty("height", "auto");
+        style.setProperty("max-height", "90vh");
     }
 
     private HistoryTokenAnchorComponent closeLink(final String id,
