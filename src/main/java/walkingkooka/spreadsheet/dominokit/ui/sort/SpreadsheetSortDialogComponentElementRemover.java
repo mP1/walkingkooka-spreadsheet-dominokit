721b7e0c0c4d2d05e7e7f6df0b88405a44e1797c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-09 12:14:46 +1000

    SpreadsheetSortDialogComponentElementRemover
    - TODO add to parent SpreadsheetSortDialogComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/sort/SpreadsheetSortDialogComponentElementRemover.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.ui.sort;
+
+import elemental2.dom.HTMLDivElement;
+import walkingkooka.collect.list.ImmutableList;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.spreadsheet.compare.SpreadsheetColumnOrRowSpreadsheetComparatorNames;
+import walkingkooka.spreadsheet.compare.SpreadsheetColumnOrRowSpreadsheetComparatorNamesList;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorNameAndDirection;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.ui.HtmlElementComponent;
+import walkingkooka.spreadsheet.dominokit.ui.card.SpreadsheetCard;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.TreePrintable;
+
+import java.util.Optional;
+
+/**
+ * A container that presents LINKS each missing a component from the parent {@link SpreadsheetColumnOrRowSpreadsheetComparatorNames}.
+ */
+final class SpreadsheetSortDialogComponentElementRemover implements HtmlElementComponent<HTMLDivElement, SpreadsheetSortDialogComponentElementRemover>,
+        TreePrintable {
+
+    /**
+     * Creates an empty {@link SpreadsheetSortDialogComponentElementRemover}.
+     */
+    static SpreadsheetSortDialogComponentElementRemover empty(final int index) {
+        return new SpreadsheetSortDialogComponentElementRemover(index);
+    }
+
+    private SpreadsheetSortDialogComponentElementRemover(final int index) {
+        this.parent = SpreadsheetCard.empty();
+
+        this.index = index;
+    }
+
+    /**
+     * Clears existing links and recreates a link that will remove an individual component of the parent {@link SpreadsheetColumnOrRowSpreadsheetComparatorNames}.
+     */
+    void refresh(final SpreadsheetColumnOrRowSpreadsheetComparatorNamesList namesList,
+                 final SpreadsheetSortDialogComponentContext context) {
+
+        final SpreadsheetCard parent = this.parent;
+        parent.clear();
+
+        final HistoryToken historyToken = context.historyToken();
+
+        final int index = this.index;
+        final SpreadsheetColumnOrRowSpreadsheetComparatorNames names = namesList.get(index);
+        final String idPrefix = SpreadsheetSortDialogComponent.ID_PREFIX + index + '-';
+
+        int i = 0;
+        final ImmutableList<SpreadsheetComparatorNameAndDirection> nameAndDirections = Lists.immutable(
+                names.comparatorNameAndDirections()
+        );
+
+        if (nameAndDirections.size() > 1) {
+            for (final SpreadsheetComparatorNameAndDirection comparatorNameAndDirections : names.comparatorNameAndDirections()) {
+
+                final SpreadsheetColumnOrRowSpreadsheetComparatorNames removed = names.setComparatorNameAndDirections(
+                        nameAndDirections.removeAtIndex(i)
+                );
+
+                // replace the old names at $index with $removed
+                parent.append(
+                        historyToken.link(idPrefix + "-remove-" + i)
+                                .setTextContent(comparatorNameAndDirections.toString())
+                                .setHistoryToken(
+                                        Optional.of(
+                                                historyToken.setSave(
+                                                        namesList.replace(
+                                                                index,
+                                                                removed
+                                                        ).text()
+                                                )
+                                        )
+                                )
+                );
+
+                i++;
+            }
+        }
+    }
+
+    /**
+     * An index for this {@link SpreadsheetColumnOrRowSpreadsheetComparatorNames} within a {@link SpreadsheetColumnOrRowSpreadsheetComparatorNamesList}.
+     */
+    private final int index;
+
+    // HtmlElementComponent.............................................................................................
+
+    @Override
+    public HTMLDivElement element() {
+        return this.parent.element();
+    }
+
+    /**
+     * The parent holding all the current links to remove individual components of a {@link SpreadsheetColumnOrRowSpreadsheetComparatorNames}
+     */
+    private final SpreadsheetCard parent;
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        printer.println(this.getClass().getSimpleName());
+        printer.indent();
+        {
+            this.parent.printTree(printer);
+        }
+        printer.outdent();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/ui/sort/SpreadsheetSortDialogComponentElementRemoverTest.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.ui.sort;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.net.UrlFragment;
+import walkingkooka.reflect.ClassTesting;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.compare.SpreadsheetColumnOrRowSpreadsheetComparatorNamesList;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.text.printer.TreePrintableTesting;
+
+public final class SpreadsheetSortDialogComponentElementRemoverTest implements ClassTesting<SpreadsheetSortDialogComponentElementRemover>,
+        TreePrintableTesting {
+
+    // cell.............................................................................................................
+
+    @Test
+    public void testCellOneNameOnly() {
+        this.refreshAndCheck(
+                0,
+                SpreadsheetColumnOrRowSpreadsheetComparatorNamesList.parse("A=text"),
+                "/1/spreadsheetName23/cell/A1:B2/sort/edit/A=text",
+                "SpreadsheetSortDialogComponentElementRemover\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n"
+        );
+    }
+
+    @Test
+    public void testCellTwoNameFirstOnly() {
+        this.refreshAndCheck(
+                0,
+                SpreadsheetColumnOrRowSpreadsheetComparatorNamesList.parse("A=text"),
+                "/1/spreadsheetName23/cell/A1:B2/sort/edit/IGNORED",
+                "SpreadsheetSortDialogComponentElementRemover\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n"
+        );
+    }
+
+    @Test
+    public void testCellTwoNameLastOnly() {
+        this.refreshAndCheck(
+                0,
+                SpreadsheetColumnOrRowSpreadsheetComparatorNamesList.parse("A=text"),
+                "/1/spreadsheetName23/cell/A1:B2/sort/edit/IGNORED",
+                "SpreadsheetSortDialogComponentElementRemover\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n"
+        );
+    }
+
+    @Test
+    public void testCellTwoNameFirstSeveral() {
+        this.refreshAndCheck(
+                0,
+                SpreadsheetColumnOrRowSpreadsheetComparatorNamesList.parse("A=text1,text2;B=ignored1,ignored2"),
+                "/1/spreadsheetName23/cell/A1:B2/sort/edit/IGNORED",
+                "SpreadsheetSortDialogComponentElementRemover\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n" +
+                        "      \"text1\" [#/1/spreadsheetName23/cell/A1:B2/bottom-right/sort/save/A%3Dtext2%3BB%3Dignored1%2Cignored2]\n" +
+                        "      \"text2\" [#/1/spreadsheetName23/cell/A1:B2/bottom-right/sort/save/A%3Dtext1%3BB%3Dignored1%2Cignored2]"
+        );
+    }
+
+    @Test
+    public void testCellTwoNameLastSeveral() {
+        this.refreshAndCheck(
+                1,
+                SpreadsheetColumnOrRowSpreadsheetComparatorNamesList.parse("A=ignored1,ignored2;B=text1,text2"),
+                "/1/spreadsheetName23/cell/A1:B2/sort/edit/IGNORED",
+                "SpreadsheetSortDialogComponentElementRemover\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n" +
+                        "      \"text1\" [#/1/spreadsheetName23/cell/A1:B2/bottom-right/sort/save/A%3Dignored1%2Cignored2%3BB%3Dtext2]\n" +
+                        "      \"text2\" [#/1/spreadsheetName23/cell/A1:B2/bottom-right/sort/save/A%3Dignored1%2Cignored2%3BB%3Dtext1]"
+        );
+    }
+
+    @Test
+    public void testCellTwoNameLastSeveral2() {
+        this.refreshAndCheck(
+                2,
+                SpreadsheetColumnOrRowSpreadsheetComparatorNamesList.parse("A=day-of-month;B=month-of-year;C=text1,text2,text3"),
+                "/1/spreadsheetName23/cell/A1:C3/sort/edit/IGNORED",
+                "SpreadsheetSortDialogComponentElementRemover\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n" +
+                        "      \"text1\" [#/1/spreadsheetName23/cell/A1:C3/bottom-right/sort/save/A%3Dday-of-month%3BB%3Dmonth-of-year%3BC%3Dtext2%2Ctext3]\n" +
+                        "      \"text2\" [#/1/spreadsheetName23/cell/A1:C3/bottom-right/sort/save/A%3Dday-of-month%3BB%3Dmonth-of-year%3BC%3Dtext1%2Ctext3]\n" +
+                        "      \"text3\" [#/1/spreadsheetName23/cell/A1:C3/bottom-right/sort/save/A%3Dday-of-month%3BB%3Dmonth-of-year%3BC%3Dtext1%2Ctext2]"
+        );
+    }
+
+
+    private void refreshAndCheck(final int index,
+                                 final SpreadsheetColumnOrRowSpreadsheetComparatorNamesList namesList,
+                                 final String historyToken,
+                                 final String expected) {
+        this.refreshAndCheck(
+                index,
+                namesList,
+                new FakeSpreadsheetSortDialogComponentContext() {
+
+                    @Override
+                    public HistoryToken historyToken() {
+                        return HistoryToken.parse(
+                                UrlFragment.parse(historyToken)
+                        );
+                    }
+                },
+                expected
+        );
+    }
+
+    private void refreshAndCheck(final int index,
+                                 final SpreadsheetColumnOrRowSpreadsheetComparatorNamesList namesList,
+                                 final SpreadsheetSortDialogComponentContext context,
+                                 final String expected) {
+        final SpreadsheetSortDialogComponentElementRemover remover = SpreadsheetSortDialogComponentElementRemover.empty(index);
+
+        remover.refresh(
+                namesList,
+                context
+        );
+
+        this.treePrintAndCheck(
+                remover,
+                expected
+        );
+    }
+
+    @Override
+    public Class<SpreadsheetSortDialogComponentElementRemover> type() {
+        return SpreadsheetSortDialogComponentElementRemover.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+}
