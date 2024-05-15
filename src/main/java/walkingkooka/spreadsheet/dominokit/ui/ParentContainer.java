77ef5e5823bfe29492bde7e27b418cf058abd658
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-15 18:54:18 +1000

    ParentContainer

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/ParentContainer.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.ui;
+
+import elemental2.dom.Element;
+import org.dominokit.domino.ui.IsElement;
+
+import java.util.List;
+
+/**
+ * Defines methods for a {@link Component} that has children.
+ */
+public interface ParentContainer<C extends Component<E>, E extends Element> {
+
+    /**
+     * Appends a new child.
+     */
+    C appendChild(final IsElement<?> child);
+
+    /**
+     * Getter that returns the child at index.
+     */
+    default IsElement<?> child(final int index) {
+        return this.children()
+                .get(index);
+    }
+
+    /**
+     * Removes an existing child.
+     */
+    C removeChild(final int index);
+
+    /**
+     * Removes all children.
+     */
+    default C clearChildren() {
+        final int count = this.children()
+                .size();
+        for (int i = 0; i < count; i++) {
+            this.removeChild(0);
+        }
+
+        return (C) this;
+    }
+
+    /**
+     * Getter that returns all children.
+     */
+    List<IsElement<?>> children();
+}
