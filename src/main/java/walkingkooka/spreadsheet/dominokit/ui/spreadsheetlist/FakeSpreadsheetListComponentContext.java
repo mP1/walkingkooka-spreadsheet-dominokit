15cb26c25b5bb371f9ba358d129f27dd940cd8fb
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-17 07:08:04 +1000

    FakeSpreadsheetListComponentContext

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/spreadsheetlist/FakeSpreadsheetListComponentContext.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.ui.spreadsheetlist;
+
+import walkingkooka.spreadsheet.dominokit.history.FakeHistoryTokenContext;
+import walkingkooka.spreadsheet.dominokit.net.SpreadsheetMetadataFetcher;
+import walkingkooka.spreadsheet.dominokit.net.SpreadsheetMetadataFetcherWatcher;
+
+import java.util.Locale;
+
+public class FakeSpreadsheetListComponentContext extends FakeHistoryTokenContext
+        implements SpreadsheetListComponentContext {
+
+    @Override
+    public Locale locale() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Runnable addSpreadsheetMetadataFetcherWatcher(final SpreadsheetMetadataFetcherWatcher watcher) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Runnable addSpreadsheetMetadataFetcherWatcherOnce(final SpreadsheetMetadataFetcherWatcher watcher) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SpreadsheetMetadataFetcher spreadsheetMetadataFetcher() {
+        throw new UnsupportedOperationException();
+    }
+}
