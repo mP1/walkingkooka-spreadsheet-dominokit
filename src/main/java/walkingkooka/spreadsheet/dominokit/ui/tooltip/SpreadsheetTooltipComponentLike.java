517cda4dfc797dd9f7a0cfee338e4208dc17ed25
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-19 22:04:12 +1000

    SpreadsheetTooltipComponentLike

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/tooltip/SpreadsheetTooltipComponentLike.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.ui.tooltip;
+
+import walkingkooka.text.CharSequences;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.TreePrintable;
+
+/**
+ * Defines a public contract for a {@link SpreadsheetTooltipComponent}.
+ */
+public interface SpreadsheetTooltipComponentLike extends TreePrintable {
+
+    SpreadsheetTooltipComponentLike setTextContent(final String text);
+
+    /**
+     * Removes the tooltip.
+     */
+    void detach();
+
+    // TreePrintable....................................................................................................
+
+    default void printTreeText(final String text,
+                               final IndentingPrinter printer) {
+        printer.println(this.getClass().getSimpleName());
+        printer.indent();
+        {
+            printer.println(
+                    CharSequences.quoteAndEscape(text)
+            );
+        }
+    }
+}
