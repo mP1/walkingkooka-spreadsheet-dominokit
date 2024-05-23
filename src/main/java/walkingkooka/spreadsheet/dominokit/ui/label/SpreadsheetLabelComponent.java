e0eb941312b0777385f605a8339394d8e0d21910
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-23 19:44:02 +1000

    SpreadsheetLabelComponent initial validate

--- src/main/java/walkingkooka/spreadsheet/dominokit/ui/label/SpreadsheetLabelComponent.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/ui/label/SpreadsheetLabelComponent.java
@@ -61,6 +61,7 @@ public final class SpreadsheetLabelComponent implements ValueComponent<HTMLField
         suggestBox.setAutoValidation(true);
 
         this.required();
+        this.validate();
     }
 
     // id...............................................................................................................
