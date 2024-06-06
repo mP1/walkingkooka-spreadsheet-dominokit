3a1a74f1f4f7f65207563fe2541166490f4ed6fd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-06-06 10:58:57 +1000

    https://github.com/mP1/walkingkooka-spreadsheet/pull/4200-4208 SpreadsheetMetadata.XXX_FORMATTER replaces XXX_FORMAT_PATTERN 2/2
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/4200
    - SpreadsheetMetadata.XXX_FORMATTER replaces XXX_FORMAT_PATTERN
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/4201
    - SpreadsheetPattern.DEFAULT_TEXT_FORMAT_PATTERN was DEFAULT_TEXT_FORMATTER
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/4202
    - SpreadsheetCell.NO_FORMAT was NO_FORMATTER
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/4203
    - SpreadsheetCell.NO_FORMAT_PATTERN was NO_FORMAT
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/4204
    - SpreadsheetUrlFragments.FORMATTER was FORMAT_PATTERN
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/4208
    - SpreadsheetCell.formatter SpreadsheetFormatterSelector was SpreadsheetFormatPattern
    
    - SpreadsheetMetadataPanelComponent removed(commented out) formatter links
    - TODO SpreadsheetCellPatternHistoryToken common parent for formatter & parse-patterns needs to be disconnected.
    - Introduced SpreadsheetPatternDialogComponentContext.savePatternText to special case formatter / pattern text stuff.

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/history/SpreadsheetCellSaveFormatPatternHistoryToken.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.history;
+
+import walkingkooka.collect.map.Maps;
+import walkingkooka.net.UrlFragment;
+import walkingkooka.spreadsheet.SpreadsheetId;
+import walkingkooka.spreadsheet.SpreadsheetName;
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.spreadsheet.format.pattern.SpreadsheetFormatPattern;
+import walkingkooka.spreadsheet.reference.AnchoredSpreadsheetSelection;
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * This {@link HistoryToken} is used by to paste a {@link SpreadsheetFormatPattern} for many cells over another range.
+ */
+public final class SpreadsheetCellSaveFormatPatternHistoryToken extends SpreadsheetCellSaveMapHistoryToken<Optional<SpreadsheetFormatPattern>> {
+
+    static SpreadsheetCellSaveFormatPatternHistoryToken with(final SpreadsheetId id,
+                                                             final SpreadsheetName name,
+                                                             final AnchoredSpreadsheetSelection anchoredSelection,
+                                                             final Map<SpreadsheetCellReference, Optional<SpreadsheetFormatPattern>> value) {
+        return new SpreadsheetCellSaveFormatPatternHistoryToken(
+                id,
+                name,
+                anchoredSelection,
+                Maps.immutable(value)
+        );
+    }
+
+    private SpreadsheetCellSaveFormatPatternHistoryToken(final SpreadsheetId id,
+                                                         final SpreadsheetName name,
+                                                         final AnchoredSpreadsheetSelection anchoredSelection,
+                                                         final Map<SpreadsheetCellReference, Optional<SpreadsheetFormatPattern>> value) {
+        super(
+                id,
+                name,
+                anchoredSelection,
+                value
+        );
+    }
+
+    @Override //
+    Optional<Class<Optional<SpreadsheetFormatPattern>>> valueType() {
+        return Optional.empty(); // polumorphic not fixed
+    }
+
+    @Override //
+    SpreadsheetCellSaveFormatPatternHistoryToken replace(final SpreadsheetId id,
+                                                         final SpreadsheetName name,
+                                                         final AnchoredSpreadsheetSelection anchoredSelection,
+                                                         final Map<SpreadsheetCellReference, Optional<SpreadsheetFormatPattern>> value) {
+        return new SpreadsheetCellSaveFormatPatternHistoryToken(
+                id,
+                name,
+                anchoredSelection,
+                value
+        );
+    }
+
+    // HasUrlFragment..................................................................................................
+
+    @Override
+    UrlFragment saveEntityUrlFragment() {
+        return FORMAT_PATTERN;
+    }
+
+    private final static UrlFragment FORMAT_PATTERN = UrlFragment.parse("format-pattern");
+
+    // HistoryTokenWatcher..............................................................................................
+
+    @Override
+    void onHistoryTokenChange0(final HistoryToken previous,
+                               final AppContext context) {
+        context.spreadsheetDeltaFetcher()
+                .patchCellsFormatPattern(
+                        this.id(),
+                        this.anchoredSelection().selection(),
+                        this.value()
+                );
+        context.pushHistoryToken(previous);
+    }
+}
