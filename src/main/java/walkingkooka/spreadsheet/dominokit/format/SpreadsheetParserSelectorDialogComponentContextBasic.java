a645f38ddded4362c2d75c61af40b337f12500f9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-01 08:10:21 +1000

    SpreadsheetParserSelectorDialogComponentContextBasic implements SpreadsheetFormatterContextDelegator

--- src/main/java/walkingkooka/spreadsheet/dominokit/format/SpreadsheetParserSelectorDialogComponentContextBasic.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/format/SpreadsheetParserSelectorDialogComponentContextBasic.java
@@ -17,10 +17,6 @@
 
 package walkingkooka.spreadsheet.dominokit.format;
 
-import walkingkooka.Either;
-import walkingkooka.color.Color;
-import walkingkooka.convert.Converter;
-import walkingkooka.spreadsheet.convert.SpreadsheetConverterContext;
 import walkingkooka.spreadsheet.dominokit.AppContext;
 import walkingkooka.spreadsheet.dominokit.history.HistoryTokenContext;
 import walkingkooka.spreadsheet.dominokit.history.HistoryTokenContextDelegator;
@@ -28,9 +24,9 @@ import walkingkooka.spreadsheet.dominokit.log.LoggingContext;
 import walkingkooka.spreadsheet.dominokit.log.LoggingContextDelegator;
 import walkingkooka.spreadsheet.dominokit.net.SpreadsheetDeltaFetcherWatcher;
 import walkingkooka.spreadsheet.dominokit.net.SpreadsheetMetadataFetcherWatcher;
-import walkingkooka.spreadsheet.format.SpreadsheetColorName;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatter;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterContext;
+import walkingkooka.spreadsheet.format.SpreadsheetFormatterContextDelegator;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterInfo;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterName;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterSample;
@@ -41,15 +37,8 @@ import walkingkooka.spreadsheet.parser.SpreadsheetParserInfo;
 import walkingkooka.spreadsheet.parser.SpreadsheetParserName;
 import walkingkooka.spreadsheet.parser.SpreadsheetParserSelector;
 import walkingkooka.spreadsheet.parser.SpreadsheetParserSelectorTextComponent;
-import walkingkooka.spreadsheet.reference.SpreadsheetLabelName;
-import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
-import walkingkooka.tree.expression.ExpressionNumberKind;
-import walkingkooka.tree.text.TextNode;
 
-import java.math.MathContext;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
 
@@ -58,7 +47,8 @@ import java.util.Set;
  */
 abstract class SpreadsheetParserSelectorDialogComponentContextBasic implements SpreadsheetParserSelectorDialogComponentContext,
         HistoryTokenContextDelegator,
-        LoggingContextDelegator {
+        LoggingContextDelegator,
+        SpreadsheetFormatterContextDelegator {
 
     SpreadsheetParserSelectorDialogComponentContextBasic(final AppContext context) {
         super();
@@ -69,185 +59,8 @@ abstract class SpreadsheetParserSelectorDialogComponentContextBasic implements S
     // SpreadsheetFormatterContext......................................................................................
 
     @Override
-    public final int cellCharacterWidth() {
-        return this.context.cellCharacterWidth();
-    }
-
-    @Override
-    public final Optional<Color> colorNumber(final int number) {
-        return this.context.colorNumber(number);
-    }
-
-    @Override
-    public final Optional<Color> colorName(final SpreadsheetColorName name) {
-        return this.context.colorName(name);
-    }
-
-    @Override
-    public final Optional<TextNode> format(final Object value) {
-        return this.context.format(value);
-    }
-
-    @Override
-    public final TextNode formatOrEmptyText(final Object value) {
-        return this.context.formatOrEmptyText(value);
-    }
-
-    @Override
-    public final int generalFormatNumberDigitCount() {
-        return this.context.generalFormatNumberDigitCount();
-    }
-
-    @Override
-    public final long dateOffset() {
-        return this.context.dateOffset();
-    }
-
-    @Override
-    public final boolean canConvert(final Object value,
-                                    final Class<?> type) {
-        return this.context.canConvert(
-                value,
-                type
-        );
-    }
-
-    @Override
-    public final <T> Either<T, String> convert(final Object value,
-                                               final Class<T> type) {
-        return this.context.convert(
-                value,
-                type
-        );
-    }
-
-    @Override
-    public final List<String> ampms() {
-        return this.context.ampms();
-    }
-
-    @Override
-    public final String ampm(final int hourOfDay) {
-        return this.context.ampm(hourOfDay);
-    }
-
-    @Override public final List<String> monthNames() {
-        return this.context.monthNames();
-    }
-
-    @Override
-    public final String monthName(final int month) {
-        return this.context.monthName(month);
-    }
-
-    @Override
-    public final List<String> monthNameAbbreviations() {
-        return this.context.monthNameAbbreviations();
-    }
-
-    @Override
-    public final String monthNameAbbreviation(final int month) {
-        return this.context.monthNameAbbreviation(month);
-    }
-
-    @Override
-    public final List<String> weekDayNames() {
-        return this.context.weekDayNames();
-    }
-
-    @Override
-    public final String weekDayName(final int day) {
-        return this.context.weekDayName(day);
-    }
-
-    @Override
-    public final List<String> weekDayNameAbbreviations() {
-        return this.context.weekDayNameAbbreviations();
-    }
-
-    @Override
-    public final String weekDayNameAbbreviation(final int day) {
-        return this.context.weekDayNameAbbreviation(day);
-    }
-
-    @Override
-    public final int defaultYear() {
-        return this.context.defaultYear();
-    }
-
-    @Override
-    public final int twoDigitYear() {
-        return this.context.twoDigitYear();
-    }
-
-    @Override
-    public final int twoToFourDigitYear(final int year) {
-        return this.context.twoToFourDigitYear(year);
-    }
-
-    @Override
-    public final Locale locale() {
-        return this.context.locale();
-    }
-
-    @Override
-    public final LocalDateTime now() {
-        return this.context.now();
-    }
-
-    @Override
-    public final String currencySymbol() {
-        return this.context.currencySymbol();
-    }
-
-    @Override
-    public final char decimalSeparator() {
-        return this.context.decimalSeparator();
-    }
-
-    @Override
-    public final String exponentSymbol() {
-        return this.context.exponentSymbol();
-    }
-
-    @Override
-    public final char groupSeparator() {
-        return this.context.groupSeparator();
-    }
-
-    @Override
-    public final char percentageSymbol() {
-        return this.context.percentageSymbol();
-    }
-
-    @Override
-    public final char negativeSign() {
-        return this.context.negativeSign();
-    }
-
-    @Override
-    public final char positiveSign() {
-        return this.context.positiveSign();
-    }
-
-    @Override
-    public final MathContext mathContext() {
-        return this.context.mathContext();
-    }
-
-    @Override
-    public final ExpressionNumberKind expressionNumberKind() {
-        return this.context.expressionNumberKind();
-    }
-
-    @Override
-    public final Converter<SpreadsheetConverterContext> converter() {
-        return this.context.converter();
-    }
-
-    @Override
-    public final SpreadsheetSelection resolveLabel(final SpreadsheetLabelName name) {
-        return this.context.resolveLabel(name);
+    public final SpreadsheetFormatterContext spreadsheetFormatterContext() {
+        return this.context;
     }
 
     // SpreadsheetParserContext.........................................................................................
