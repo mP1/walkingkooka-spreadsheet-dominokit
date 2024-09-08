f092ad23255581866dd219d44ea10eb4711b9639
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-09-08 20:06:53 +1000

    ConverterInfoSetComponent

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/convert/ConverterInfoSetComponentTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.convert;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.convert.provider.ConverterInfoSet;
+import walkingkooka.convert.provider.ConverterProviders;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.reflect.JavaVisibility;
+
+public final class ConverterInfoSetComponentTest implements ClassTesting2<ConverterInfoSetComponent> {
+
+    @Test
+    public void testParseAndText() {
+        final ConverterInfoSet infos = ConverterInfoSet.with(
+                ConverterProviders.converters()
+                        .converterInfos()
+        );
+
+        this.checkEquals(
+                infos,
+                ConverterInfoSet.parse(infos.text())
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<ConverterInfoSetComponent> type() {
+        return ConverterInfoSetComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
