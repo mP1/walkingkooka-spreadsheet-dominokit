667ca2207b37807631075f86b1125f5babee30b1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-11-06 17:35:22 +1100

    SpreadsheetAppLayout.prepare was App.prepareLayout

--- src/main/java/walkingkooka/spreadsheet/dominokit/App.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/App.java
@@ -19,13 +19,7 @@ package walkingkooka.spreadsheet.dominokit;
 
 import com.google.gwt.core.client.EntryPoint;
 import elemental2.dom.DomGlobal;
-import elemental2.dom.Event;
 import elemental2.dom.Headers;
-import org.dominokit.domino.ui.cards.Card;
-import org.dominokit.domino.ui.elements.SectionElement;
-import org.dominokit.domino.ui.icons.lib.Icons;
-import org.dominokit.domino.ui.layout.AppLayout;
-import org.dominokit.domino.ui.layout.RightDrawerSize;
 import walkingkooka.convert.provider.ConverterInfoSet;
 import walkingkooka.convert.provider.ConverterProvider;
 import walkingkooka.convert.provider.ConverterProviders;
@@ -50,7 +44,6 @@ import walkingkooka.spreadsheet.dominokit.clipboard.ClipboardContextReadWatcher;
 import walkingkooka.spreadsheet.dominokit.clipboard.ClipboardContextWriteWatcher;
 import walkingkooka.spreadsheet.dominokit.clipboard.ClipboardContexts;
 import walkingkooka.spreadsheet.dominokit.clipboard.ClipboardTextItem;
-import walkingkooka.spreadsheet.dominokit.dialog.SpreadsheetDialogComponent;
 import walkingkooka.spreadsheet.dominokit.focus.CanGiveFocus;
 import walkingkooka.spreadsheet.dominokit.focus.CanGiveFocuses;
 import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
@@ -62,8 +55,6 @@ import walkingkooka.spreadsheet.dominokit.history.SpreadsheetListSelectHistoryTo
 import walkingkooka.spreadsheet.dominokit.log.LoggingContext;
 import walkingkooka.spreadsheet.dominokit.log.LoggingContextDelegator;
 import walkingkooka.spreadsheet.dominokit.log.LoggingContexts;
-import walkingkooka.spreadsheet.dominokit.meta.SpreadsheetMetadataPanelComponent;
-import walkingkooka.spreadsheet.dominokit.meta.SpreadsheetMetadataPanelComponentContexts;
 import walkingkooka.spreadsheet.dominokit.net.ConverterFetcher;
 import walkingkooka.spreadsheet.dominokit.net.ConverterFetcherWatcher;
 import walkingkooka.spreadsheet.dominokit.net.ConverterFetcherWatchers;
@@ -92,7 +83,6 @@ import walkingkooka.spreadsheet.dominokit.net.SpreadsheetMetadataFetcherWatchers
 import walkingkooka.spreadsheet.dominokit.net.SpreadsheetParserFetcher;
 import walkingkooka.spreadsheet.dominokit.net.SpreadsheetParserFetcherWatcher;
 import walkingkooka.spreadsheet.dominokit.net.SpreadsheetParserFetcherWatchers;
-import walkingkooka.spreadsheet.dominokit.toolbar.SpreadsheetToolbarComponent;
 import walkingkooka.spreadsheet.dominokit.viewport.SpreadsheetViewportCache;
 import walkingkooka.spreadsheet.dominokit.viewport.SpreadsheetViewportComponent;
 import walkingkooka.spreadsheet.engine.SpreadsheetDelta;
@@ -292,7 +282,14 @@ public class App implements EntryPoint,
                 this
         );
 
-        this.layout = this.prepareLayout();
+        this.layout = SpreadsheetAppLayout.prepare(
+                this.viewportComponent,
+                this
+        );
+
+        DomGlobal.document.body.append(
+                this.layout.element()
+        );
     }
 
     // EntryPoint.......................................................................................................
@@ -304,99 +301,6 @@ public class App implements EntryPoint,
         );
 
         this.fireWindowSizeLater(this::onWindowResize);
-    }
-
-    // SpreadsheetAppLayout.............................................................................................
-
-    // header = metadata toggle | clickable(editable) spreadsheet name
-    // right = editable metadata properties
-    // content = toolbar
-    //   formula,
-    //   table holding spreadsheet cells
-    private SpreadsheetAppLayout prepareLayout() {
-        final SpreadsheetAppLayout layout = SpreadsheetAppLayout.empty(this);
-
-        layout.setOverFlowX("hidden")
-                .setOverFlowY("hidden");
-
-        layout.getContent()
-                .setPadding("0px") // kills the dui-layout-content padding: 25px
-                .setOverFlowY("hidden") // stop scrollbars on the cell viewport
-                .appendChild(this.viewportComponent);
-
-        layout.getNavBar()
-                .withTitle(
-                        (n, header) -> {
-                            header.appendChild(
-                                    AppHistoryTokenAnchorComponents.files()
-                            );
-                            header.appendChild(
-                                    AppHistoryTokenAnchorComponents.spreadsheetName(this)
-                            );
-                        }
-                ).getBody()
-                .appendChild(
-                        SpreadsheetToolbarComponent.with(this)
-                );
-
-        layout.setRightDrawerSize(RightDrawerSize.XLARGE)
-                .getRightDrawerContent()
-                .appendChild(
-                        Card.create()
-                                .appendChild(
-                                        SpreadsheetMetadataPanelComponent.with(
-                                                SpreadsheetAppLayoutRightDrawerComponent.with(layout),
-                                                SpreadsheetMetadataPanelComponentContexts.appContext(this)
-                                        )
-                                )
-                );
-        layout.onRightDrawerClosed(
-                this::appLayoutRightPanelClosed
-        );
-
-        layout.setRightDrawerToggleIcon(
-                Icons.menu_open()
-                        .addClickListener(
-                                this::appLayoutRightToggleIconOnClick
-                        )
-        );
-
-        DomGlobal.document.body.append(
-                layout.element()
-        );
-
-        return layout;
-    }
-
-    /**
-     * Handler that reacts to the right panel toggle icon being clicked, updating the history. The {@link SpreadsheetMetadataPanelComponent}
-     * will see the history token change and then open or hide itself.
-     */
-    private void appLayoutRightToggleIconOnClick(final Event event) {
-        HistoryToken token = this.historyToken();
-
-        this.pushHistoryToken(
-                this.layout.isRightDrawerOpen() ?
-                        token.metadataHide() :
-                        token.metadataShow()
-        );
-    }
-
-    private final SpreadsheetAppLayout layout;
-
-    /**
-     * This event is fired when the right panel closes, such as when the user clicks away from it and the history token needs to be updated.
-     */
-    private void appLayoutRightPanelClosed(final AppLayout layout,
-                                           final SectionElement section) {
-        final HistoryToken token = this.historyToken();
-
-        // HACK only hide metadata panel if NOT displaying a metadata editor dialog
-        if (false == SpreadsheetDialogComponent.isAnyOpen()) {
-            this.pushHistoryToken(
-                    token.metadataHide()
-            );
-        }
     }
 
     // WindowResize.....................................................................................................
@@ -418,6 +322,8 @@ public class App implements EntryPoint,
 
         this.computeAndSaveSpreadsheetListDialogComponentDefaultCount(newHeight);
     }
+
+    private final SpreadsheetAppLayout layout;
 
     // defaultCount.....................................................................................................
 
