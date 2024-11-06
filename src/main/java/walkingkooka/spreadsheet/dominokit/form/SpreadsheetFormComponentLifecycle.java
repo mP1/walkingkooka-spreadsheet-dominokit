81c35146135fe8be9235098e0a9382f74e0496f9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-11-06 20:45:29 +1100

    SpreadsheetFormComponentLifecycle

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/form/SpreadsheetFormComponentLifecycle.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.form;
+
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.text.printer.TreePrintable;
+
+/**
+ * A form holds form components such as text boxes, links etc. Note it does not receive {@link walkingkooka.spreadsheet.dominokit.history.HistoryTokenWatcher}
+ * events, it instead receives events to prepare or {@link #giveFocus(AppContext)} and {@link #refresh(AppContext)}.
+ */
+public interface SpreadsheetFormComponentLifecycle extends TreePrintable {
+
+    void giveFocus(final AppContext context);
+
+    void refresh(final AppContext context);
+}
