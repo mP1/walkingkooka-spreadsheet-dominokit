5e50cfa9141a6ce3d377163bfd738e4f973b214c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-11-13 14:55:02 +1100

    SpreadsheetFormatterAliasSetComponentTest implements ValueComponentTesting was ClassTesting

--- src/test/java/walkingkooka/spreadsheet/dominokit/format/SpreadsheetFormatterAliasSetComponentTest.java
+++ src/test/java/walkingkooka/spreadsheet/dominokit/format/SpreadsheetFormatterAliasSetComponentTest.java
@@ -17,12 +17,15 @@
 
 package walkingkooka.spreadsheet.dominokit.format;
 
+import elemental2.dom.HTMLFieldSetElement;
 import org.junit.jupiter.api.Test;
-import walkingkooka.reflect.ClassTesting2;
 import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.value.ValueComponentTesting;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterAliasSet;
 
-public final class SpreadsheetFormatterAliasSetComponentTest implements ClassTesting2<SpreadsheetFormatterAliasSetComponent> {
+import java.util.Optional;
+
+public final class SpreadsheetFormatterAliasSetComponentTest implements ValueComponentTesting<HTMLFieldSetElement, SpreadsheetFormatterAliasSet, SpreadsheetFormatterAliasSetComponent> {
 
     @Test
     public void testParseAndText() {
@@ -31,6 +34,40 @@ public final class SpreadsheetFormatterAliasSetComponentTest implements ClassTes
         this.checkEquals(
                 alias,
                 SpreadsheetFormatterAliasSet.parse(alias.text())
+        );
+    }
+
+    @Test
+    public void testSetStringValue() {
+        this.treePrintAndCheck(
+                SpreadsheetFormatterAliasSetComponent.empty()
+                        .setStringValue(
+                                Optional.of(
+                                        "alias1 formatter1, formatter2"
+                                )
+                        ),
+                "SpreadsheetFormatterAliasSetComponent\n" +
+                        "  ValueSpreadsheetTextBox\n" +
+                        "    SpreadsheetTextBox\n" +
+                        "      [alias1 formatter1, formatter2]\n"
+        );
+    }
+
+    @Test
+    public void testSetStringValueWithInvalid() {
+        this.treePrintAndCheck(
+                SpreadsheetFormatterAliasSetComponent.empty()
+                        .setStringValue(
+                                Optional.of(
+                                        "alias1 formatter1, 9"
+                                )
+                        ),
+                "SpreadsheetFormatterAliasSetComponent\n" +
+                        "  ValueSpreadsheetTextBox\n" +
+                        "    SpreadsheetTextBox\n" +
+                        "      [alias1 formatter1, 9]\n" +
+                        "      Errors\n" +
+                        "        Invalid character '9' at 19\n"
         );
     }
 
