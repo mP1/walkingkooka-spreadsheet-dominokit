1f3d353650ececee14e3f9b3f7bef0dae196a6eb
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-12-06 13:03:25 +1100

    SpreadsheetCellRangeReferencePathComponentTest.testSetValue

--- src/test/java/walkingkooka/spreadsheet/dominokit/find/SpreadsheetCellRangeReferencePathComponentTest.java
+++ src/test/java/walkingkooka/spreadsheet/dominokit/find/SpreadsheetCellRangeReferencePathComponentTest.java
@@ -23,10 +23,12 @@ import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.spreadsheet.dominokit.value.ValueComponentTesting;
 import walkingkooka.spreadsheet.reference.SpreadsheetCellRangeReferencePath;
 
+import java.util.Optional;
+
 public final class SpreadsheetCellRangeReferencePathComponentTest implements ValueComponentTesting<HTMLFieldSetElement, SpreadsheetCellRangeReferencePath, SpreadsheetCellRangeReferencePathComponent> {
 
     @Test
-    public void testTreePrint() {
+    public void testEmpty() {
         this.treePrintAndCheck(
                 SpreadsheetCellRangeReferencePathComponent.empty(),
                 "SpreadsheetCellRangeReferencePathComponent\n" +
@@ -42,6 +44,29 @@ public final class SpreadsheetCellRangeReferencePathComponentTest implements Val
                         "      bottom-up right-left=BURL\n"
         );
     }
+
+    @Test
+    public void testSetValue() {
+        this.treePrintAndCheck(
+                SpreadsheetCellRangeReferencePathComponent.empty()
+                        .setValue(
+                                Optional.of(SpreadsheetCellRangeReferencePath.BULR)
+                        ),
+                "SpreadsheetCellRangeReferencePathComponent\n" +
+                        "  SpreadsheetSelectComponent\n" +
+                        "    [BULR]\n" +
+                        "      left-right top-down=LRTD\n" +
+                        "      right-left top-down=RLTD\n" +
+                        "      left-right bottom-up=LRBU\n" +
+                        "      right-left bottom-up=RLBU\n" +
+                        "      top-down left-right=TDLR\n" +
+                        "      top-down right-left=TDRL\n" +
+                        "      bottom-up left-right=BULR\n" +
+                        "      bottom-up right-left=BURL\n"
+        );
+    }
+
+    // class............................................................................................................
 
     @Override
     public Class<SpreadsheetCellRangeReferencePathComponent> type() {
