734bb968ae72ee207f0f2b7bc5cd3c4a59032d40
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-12-07 08:39:57 +1100

    RemovePluginNameSetComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/pluginname/RemovePluginNameSetComponent.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "PluginNameSet IS" BPluginNameSetIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.pluginname;
+
+import elemental2.dom.HTMLDivElement;
+import elemental2.dom.Node;
+import org.dominokit.domino.ui.utils.HasChangeListeners.ChangeListener;
+import walkingkooka.plugin.PluginAliasLike;
+import walkingkooka.plugin.PluginAliasSetLike;
+import walkingkooka.plugin.PluginName;
+import walkingkooka.plugin.PluginNameSet;
+import walkingkooka.spreadsheet.dominokit.HtmlElementComponent;
+import walkingkooka.spreadsheet.dominokit.card.SpreadsheetCard;
+import walkingkooka.spreadsheet.dominokit.flex.SpreadsheetFlexLayout;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenAnchorComponent;
+import walkingkooka.text.CaseKind;
+import walkingkooka.text.CharSequences;
+import walkingkooka.text.printer.IndentingPrinter;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+/**
+ * A component that contains a panel holding links of for each present {@link PluginAliasLike}, removing each from a {@link PluginAliasSetLike}.
+ */
+public final class RemovePluginNameSetComponent implements HtmlElementComponent<HTMLDivElement, RemovePluginNameSetComponent> {
+
+    /**
+     * Creates an empty {@link RemovePluginNameSetComponent}.
+     */
+    public static RemovePluginNameSetComponent empty(final String id) {
+        return new RemovePluginNameSetComponent(
+                CharSequences.failIfNullOrEmpty(id, "id")
+        );
+    }
+
+    private RemovePluginNameSetComponent(final String id) {
+        this.id = id;
+
+        this.flex = SpreadsheetFlexLayout.row();
+        this.root = SpreadsheetCard.empty()
+                .setTitle("Remove")
+                .appendChild(this.flex);
+    }
+
+    RemovePluginNameSetComponent setFilterValueChangeListener(final ChangeListener<Optional<String>> listener) {
+        this.root.setFilterValueChangeListener(listener);
+        return this;
+    }
+
+    RemovePluginNameSetComponent setFilter(final Predicate<CharSequence> filter) {
+        this.filter = filter;
+        return this;
+    }
+
+    /**
+     * A {@link Predicate} which is used to filter links by testing the text against the given {@link Predicate}.
+     */
+    private Predicate<CharSequence> filter;
+
+    public void refresh(final PluginNameSet aliases, // value from SpreadsheetMetadata
+                        final PluginNameSet providerAliases, // list of available aliases from provider
+                        final RemovePluginNameSetComponentContext context) {
+        final SpreadsheetFlexLayout flex = this.flex.removeAllChildren();
+
+        int i = 0;
+
+        flex.appendChild(
+                this.anchor(
+                        "*",
+                        aliases,
+                        i,
+                        context
+                )
+        );
+
+        i++;
+
+        final Predicate<CharSequence> filter = this.filter;
+
+        // for each provider Alias present in $aliases add a link.
+        for (final PluginName name : providerAliases) {
+
+            if (aliases.contains(name) && (null == filter || filter.test(name.text()))) {
+                flex.appendChild(
+                        this.anchor(
+                                name,
+                                aliases.delete(name),
+                                i,
+                                context
+                        )
+                );
+
+                i++;
+            }
+        }
+
+        if (i > 1) {
+            this.root.show();
+        } else {
+            this.flex.removeChild(0); // remove "*" link
+            this.root.hide();
+        }
+    }
+
+    /**
+     * Creates an anchor which will when clicked saves the plugin without this {@link PluginName}.
+     */
+    private HistoryTokenAnchorComponent anchor(final PluginName remove,
+                                               final PluginNameSet aliases,
+                                               final int index,
+                                               final RemovePluginNameSetComponentContext context) {
+        return this.anchor(
+                CaseKind.kebabToTitle(
+                        remove.value()
+                ),
+                aliases,
+                index,
+                context
+        );
+    }
+
+    /**
+     * Creates an anchor which will when clicked saves the plugin without this {@link PluginName}.
+     */
+    private HistoryTokenAnchorComponent anchor(final String title,
+                                               final PluginNameSet aliases,
+                                               final int index,
+                                               final RemovePluginNameSetComponentContext context) {
+        return context.historyToken()
+                .saveLink(
+                        this.id + index,
+                        title,
+                        aliases.text()
+                );
+    }
+
+    /**
+     * The base id prefix
+     */
+    private final String id;
+
+    private final SpreadsheetCard root;
+
+    /**
+     * The parent holding LINKS
+     */
+    private final SpreadsheetFlexLayout flex;
+
+    // setCssText.......................................................................................................
+
+    @Override
+    public RemovePluginNameSetComponent setCssText(final String css) {
+        Objects.requireNonNull(css, "css");
+
+        this.root.setCssText(css);
+        return this;
+    }
+
+    // IsElement........................................................................................................
+
+    @Override
+    public HTMLDivElement element() {
+        return this.root.element();
+    }
+
+    // node.............................................................................................................
+
+    @Override
+    public Node node() {
+        return this.element();
+    }
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        printer.println(this.getClass().getSimpleName());
+        printer.indent();
+        {
+            this.root.printTree(printer);
+        }
+        printer.outdent();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/pluginname/RemovePluginNameSetComponentTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.pluginname;
+
+import elemental2.dom.HTMLDivElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.plugin.PluginNameSet;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.SpreadsheetId;
+import walkingkooka.spreadsheet.SpreadsheetName;
+import walkingkooka.spreadsheet.dominokit.HtmlElementComponentTesting;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+
+public final class RemovePluginNameSetComponentTest implements HtmlElementComponentTesting<RemovePluginNameSetComponent,
+        HTMLDivElement> {
+
+    private final static RemovePluginNameSetComponentContext CONTEXT = new FakeRemovePluginNameSetComponentContext() {
+        @Override
+        public HistoryToken historyToken() {
+            return HistoryToken.metadataPropertySelect(
+                    SpreadsheetId.with(1),
+                    SpreadsheetName.with("SpreadsheetName123"),
+                    SpreadsheetMetadataPropertyName.PLUGINS
+            );
+        }
+    };
+
+    @Test
+    public void testRefreshNoneRemovable() {
+        final RemovePluginNameSetComponent component = RemovePluginNameSetComponent.empty("base-id-123-remove-");
+        component.refresh(
+                PluginNameSet.EMPTY, // present
+                PluginNameSet.parse("name1, name2, name3"), // provider
+                CONTEXT
+        );
+
+        // all disabled no need to create any remove links
+        this.treePrintAndCheck(
+                component,
+                "RemovePluginNameSetComponent\n"
+        );
+    }
+
+    @Test
+    public void testRefreshSomeRemovable() {
+        final RemovePluginNameSetComponent component = RemovePluginNameSetComponent.empty("base-id-123-remove-");
+        component.refresh(
+                PluginNameSet.parse("name1, name2"), // present
+                PluginNameSet.parse("name1, name2, name3, name4"), // provider
+                this.context()
+        );
+
+        this.treePrintAndCheck(
+                component,
+                "RemovePluginNameSetComponent\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n" +
+                        "      Remove\n" +
+                        "        SpreadsheetFlexLayout\n" +
+                        "          ROW\n" +
+                        "            \"*\" [#/1/SpreadsheetName123/metadata/plugins/save/name1,name2] id=base-id-123-remove-0-Link\n" +
+                        "            \"Name1\" [#/1/SpreadsheetName123/metadata/plugins/save/name2] id=base-id-123-remove-1-Link\n" +
+                        "            \"Name2\" [#/1/SpreadsheetName123/metadata/plugins/save/name1] id=base-id-123-remove-2-Link\n"
+        );
+    }
+
+    @Test
+    public void testRefreshAllRemovable() {
+        final RemovePluginNameSetComponent component = RemovePluginNameSetComponent.empty("base-id-123-remove-");
+        component.refresh(
+                PluginNameSet.parse("name1, name2, name3"), // present
+                PluginNameSet.parse("name1, name2, name3"), // provider
+                CONTEXT
+        );
+
+        // all disabled no need to create any enable links
+        this.treePrintAndCheck(
+                component,
+                "RemovePluginNameSetComponent\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n" +
+                        "      Remove\n" +
+                        "        SpreadsheetFlexLayout\n" +
+                        "          ROW\n" +
+                        "            \"*\" [#/1/SpreadsheetName123/metadata/plugins/save/name1,name2,name3] id=base-id-123-remove-0-Link\n" +
+                        "            \"Name1\" [#/1/SpreadsheetName123/metadata/plugins/save/name2,name3] id=base-id-123-remove-1-Link\n" +
+                        "            \"Name2\" [#/1/SpreadsheetName123/metadata/plugins/save/name1,name3] id=base-id-123-remove-2-Link\n" +
+                        "            \"Name3\" [#/1/SpreadsheetName123/metadata/plugins/save/name1,name2] id=base-id-123-remove-3-Link\n"
+        );
+    }
+
+    @Test
+    public void testRefreshAllRemovableWithFilter() {
+        final RemovePluginNameSetComponent component = RemovePluginNameSetComponent.empty("base-id-123-remove-");
+        component.setFilter((t) -> t.toString().contains("name"));
+        component.refresh(
+                PluginNameSet.parse("name1, name2, missing3"), // present
+                PluginNameSet.parse("name1, name2, missing3"), // provider
+                CONTEXT
+        );
+
+        // all disabled no need to create any enable links
+        this.treePrintAndCheck(
+                component,
+                "RemovePluginNameSetComponent\n" +
+                        "  SpreadsheetCard\n" +
+                        "    Card\n" +
+                        "      Remove\n" +
+                        "        SpreadsheetFlexLayout\n" +
+                        "          ROW\n" +
+                        "            \"*\" [#/1/SpreadsheetName123/metadata/plugins/save/missing3,name1,name2] id=base-id-123-remove-0-Link\n" +
+                        "            \"Name1\" [#/1/SpreadsheetName123/metadata/plugins/save/missing3,name2] id=base-id-123-remove-1-Link\n" +
+                        "            \"Name2\" [#/1/SpreadsheetName123/metadata/plugins/save/missing3,name1] id=base-id-123-remove-2-Link\n"
+        );
+    }
+
+    private RemovePluginNameSetComponentContext context() {
+        return CONTEXT;
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<RemovePluginNameSetComponent> type() {
+        return Cast.to(RemovePluginNameSetComponent.class);
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
