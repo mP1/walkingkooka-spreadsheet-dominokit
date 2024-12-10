09cab19825969b05fdc60d86f975e6f798d9e98b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-12-10 22:28:04 +1100

    PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsersTest

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/pluginaliassetlike/PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsersTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.pluginaliassetlike;
+
+
+import walkingkooka.spreadsheet.dominokit.AppContexts;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserAlias;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserAliasSet;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserInfo;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserInfoSet;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserName;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserSelector;
+
+public final class PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsersTest implements PluginAliasSetLikeDialogComponentContextTesting<PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsers,
+        SpreadsheetParserName,
+        SpreadsheetParserInfo,
+        SpreadsheetParserInfoSet,
+        SpreadsheetParserSelector,
+        SpreadsheetParserAlias,
+        SpreadsheetParserAliasSet> {
+    @Override
+    public PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsers createContext() {
+        return PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsers.with(AppContexts.fake());
+    }
+
+    // Context..........................................................................................................
+
+    @Override
+    public String typeNameSuffix() {
+        return SpreadsheetParserAliasSet.class.getSimpleName() + "Parsers";
+    }
+
+    // Class............................................................................................................
+
+    @Override
+    public Class<PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsers> type() {
+        return PluginAliasSetLikeDialogComponentContextBasicSpreadsheetParserAliasSetParsers.class;
+    }
+}
