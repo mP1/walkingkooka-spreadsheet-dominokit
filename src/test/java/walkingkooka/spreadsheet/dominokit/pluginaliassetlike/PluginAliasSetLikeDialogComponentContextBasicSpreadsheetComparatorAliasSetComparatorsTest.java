9968e6c6c4bbb409c290e360132e8e2cef85e922
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-12-10 22:08:48 +1100

    PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparatorsTest

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/pluginaliassetlike/PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparatorsTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.pluginaliassetlike;
+
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorAlias;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorAliasSet;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorInfo;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorInfoSet;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorName;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorSelector;
+import walkingkooka.spreadsheet.dominokit.AppContexts;
+
+public final class PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparatorsTest implements PluginAliasSetLikeDialogComponentContextTesting<PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparators,
+        SpreadsheetComparatorName,
+        SpreadsheetComparatorInfo,
+        SpreadsheetComparatorInfoSet,
+        SpreadsheetComparatorSelector,
+        SpreadsheetComparatorAlias,
+        SpreadsheetComparatorAliasSet> {
+    @Override
+    public PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparators createContext() {
+        return PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparators.with(AppContexts.fake());
+    }
+
+    // Context..........................................................................................................
+
+    @Override
+    public String typeNameSuffix() {
+        return SpreadsheetComparatorAliasSet.class.getSimpleName() + "Comparators";
+    }
+
+    // Class............................................................................................................
+
+    @Override
+    public Class<PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparators> type() {
+        return PluginAliasSetLikeDialogComponentContextBasicSpreadsheetComparatorAliasSetComparators.class;
+    }
+}
