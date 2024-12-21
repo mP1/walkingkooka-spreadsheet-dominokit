515a34b703556421080fc2bbd966588e1beb368f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-12-21 19:30:16 +1100

    TextViewComponent

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/text/TextViewComponent.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.text;
+
+import elemental2.dom.HTMLDivElement;
+import elemental2.dom.Node;
+
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * A barebones component that may be used to display text. Many methods such as support for listeners etc all throw {@link UnsupportedOperationException}.
+ */
+public final class TextViewComponent implements TextViewComponentLike {
+
+    public static TextViewComponent empty() {
+        return new TextViewComponent();
+    }
+
+    private TextViewComponent() {
+        super();
+    }
+
+    @Override
+    public TextViewComponent setId(final String id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public String id() {
+        return this.id;
+    }
+
+    private String id;
+
+    @Override
+    public TextViewComponent setValue(final Optional<String> value) {
+        this.value = Objects.requireNonNull(value, "value");
+        return this;
+    }
+
+    @Override
+    public Optional<String> value() {
+        return this.value;
+    }
+
+    private Optional<String> value;
+
+    @Override
+    public TextViewComponent setCssText(final String css) {
+        return this;
+    }
+
+    @Override
+    public Node node() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public HTMLDivElement element() {
+        throw new UnsupportedOperationException();
+    }
+}
