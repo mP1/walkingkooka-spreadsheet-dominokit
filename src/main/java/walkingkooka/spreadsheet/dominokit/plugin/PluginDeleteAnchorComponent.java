a89ed91e0ab43f30af28b7ca477b6512bf25e541
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-12-25 11:32:19 +1100

    PluginDeleteAnchorComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/plugin/PluginDeleteAnchorComponent.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.plugin;
+
+import elemental2.dom.HTMLAnchorElement;
+import walkingkooka.plugin.PluginName;
+import walkingkooka.spreadsheet.dominokit.HtmlElementComponent;
+import walkingkooka.spreadsheet.dominokit.anchor.AnchorComponentLike;
+import walkingkooka.spreadsheet.dominokit.anchor.AnchorComponentLikeDelegator;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenAnchorComponent;
+import walkingkooka.spreadsheet.dominokit.history.PluginDeleteHistoryToken;
+import walkingkooka.spreadsheet.dominokit.value.ValueHistoryTokenAnchorComponent;
+
+import java.util.Optional;
+
+/**
+ * Creates a {@link PluginDeleteAnchorComponent}.
+ */
+public final class PluginDeleteAnchorComponent implements HtmlElementComponent<HTMLAnchorElement, PluginDeleteAnchorComponent>,
+        AnchorComponentLikeDelegator<PluginDeleteAnchorComponent> {
+
+    public static PluginDeleteAnchorComponent empty() {
+        return new PluginDeleteAnchorComponent();
+    }
+
+    private PluginDeleteAnchorComponent() {
+        this.component = ValueHistoryTokenAnchorComponent.with(
+                HistoryTokenAnchorComponent.empty(),
+                this::getter,
+                this::setter
+        );
+    }
+
+    /**
+     * Getter that returns the {@link PluginName}.
+     */
+    private Optional<PluginName> getter(final HistoryTokenAnchorComponent anchor) {
+        return anchor.historyToken()
+                .map(t -> t.cast(PluginDeleteHistoryToken.class).name());
+    }
+
+    private void setter(final Optional<PluginName> value,
+                        final HistoryTokenAnchorComponent anchor) {
+        anchor.setHistoryToken(
+                value.map(v -> HistoryToken.pluginDelete(v))
+        );
+    }
+
+    // AnchorComponentLikeDelegator......................................................................................
+
+    @Override
+    public AnchorComponentLike<?> anchorComponentLike() {
+        return this.component;
+    }
+
+    // HtmlElementComponent.............................................................................................
+
+    /**
+     * Setter that updates the wrapped anchor with the {@link PluginName}.
+     */
+    public PluginDeleteAnchorComponent setValue(final Optional<PluginName> value) {
+        this.component.setValue(value);
+
+        return this;
+    }
+
+    // @VisibleForTesting
+    final ValueHistoryTokenAnchorComponent<PluginName> component;
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/plugin/PluginDeleteAnchorComponentTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.plugin;
+
+import elemental2.dom.HTMLAnchorElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.plugin.PluginName;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.HtmlElementComponentTesting;
+
+import java.util.Optional;
+
+public final class PluginDeleteAnchorComponentTest implements HtmlElementComponentTesting<PluginDeleteAnchorComponent, HTMLAnchorElement> {
+
+    @Test
+    public void testClearValue() {
+        this.treePrintAndCheck(
+                PluginDeleteAnchorComponent.empty()
+                        .setId("plugin-delete-anchor-id")
+                        .setTextContent("View123!"),
+                "\"View123!\" id=plugin-delete-anchor-id"
+        );
+    }
+
+    @Test
+    public void testSetValue() {
+        final PluginName pluginName = PluginName.with("PluginName456");
+
+        this.treePrintAndCheck(
+                PluginDeleteAnchorComponent.empty()
+                        .setId("plugin-delete-anchor-id")
+                        .setTextContent("View456!")
+                        .setValue(
+                                Optional.of(
+                                        pluginName
+                                )
+                        ),
+                "\"View456!\" [#/plugin/PluginName456/delete] id=plugin-delete-anchor-id"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<PluginDeleteAnchorComponent> type() {
+        return PluginDeleteAnchorComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
