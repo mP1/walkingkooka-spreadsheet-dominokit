653ceba89e7ed5ab6b7c276c03c82b11c877f3dd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-01-02 19:33:07 +1100

    PluginUploadSaveAnchorComponent

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/plugin/PluginUploadSaveAnchorComponentTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.plugin;
+
+import elemental2.dom.HTMLAnchorElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.HtmlElementComponentTesting;
+import walkingkooka.spreadsheet.dominokit.file.BrowserFile;
+
+import java.util.Optional;
+
+public final class PluginUploadSaveAnchorComponentTest implements HtmlElementComponentTesting<PluginUploadSaveAnchorComponent, HTMLAnchorElement> {
+
+    @Test
+    public void testClearFile() {
+        this.treePrintAndCheck(
+                PluginUploadSaveAnchorComponent.empty("lost")
+                        .setId("Upload123")
+                        .setTextContent("Upload123!"),
+                "\"Upload123!\" DISABLED id=Upload123"
+        );
+    }
+
+    @Test
+    public void testSetValue() {
+        this.treePrintAndCheck(
+                PluginUploadSaveAnchorComponent.empty("lost")
+                        .setId("Upload456")
+                        .setTextContent("Upload456!")
+                        .setValue(
+                                Optional.of(
+                                        BrowserFile.base64(
+                                                "Filename123",
+                                                "Filecontent456"
+                                        )
+                                )
+                        ),
+                "\"Upload456!\" [#/plugin-upload/save/base64/Filename123/Filecontent456] id=Upload456"
+        );
+    }
+
+    @Test
+    public void testSetValueClearValue() {
+        this.treePrintAndCheck(
+                PluginUploadSaveAnchorComponent.empty("lost")
+                        .setId("Upload789")
+                        .setTextContent("Upload789!")
+                        .setValue(
+                                Optional.of(
+                                        BrowserFile.base64(
+                                                "Filename123",
+                                                "Filecontent456"
+                                        )
+                                )
+                        ).clearValue(),
+                "\"Upload789!\" DISABLED id=Upload789"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<PluginUploadSaveAnchorComponent> type() {
+        return PluginUploadSaveAnchorComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
