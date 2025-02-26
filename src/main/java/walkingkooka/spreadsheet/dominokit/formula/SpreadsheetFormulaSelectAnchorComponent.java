1cff2821f4e3d4dfa065e402960654f221ce9fc2
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-02-26 14:21:06 +1100

    SpreadsheetFormulaSelectAnchorComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/formula/SpreadsheetFormulaSelectAnchorComponent.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.formula;
+
+import walkingkooka.spreadsheet.dominokit.anchor.AnchorComponent;
+import walkingkooka.spreadsheet.dominokit.anchor.AnchorComponentDelegator;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenAnchorComponent;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenContext;
+import walkingkooka.spreadsheet.dominokit.history.SpreadsheetCellFormulaSelectHistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.SpreadsheetCellSelectHistoryToken;
+import walkingkooka.spreadsheet.dominokit.value.ValueHistoryTokenAnchorComponent;
+import walkingkooka.spreadsheet.reference.SpreadsheetExpressionReference;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Creates a {@link SpreadsheetFormulaSelectAnchorComponent}.
+ * The value which must be a {@link SpreadsheetExpressionReference} will be used to select the cell and edit the formula.
+ * The link text will contain the {@link SpreadsheetExpressionReference}.
+ */
+public final class SpreadsheetFormulaSelectAnchorComponent implements AnchorComponentDelegator<SpreadsheetFormulaSelectAnchorComponent, SpreadsheetExpressionReference> {
+
+    public static SpreadsheetFormulaSelectAnchorComponent with(final String id,
+                                                               final HistoryTokenContext context) {
+        return new SpreadsheetFormulaSelectAnchorComponent(
+            id,
+            context
+        );
+    }
+
+    private SpreadsheetFormulaSelectAnchorComponent(final String id,
+                                                    final HistoryTokenContext context) {
+        this.component = ValueHistoryTokenAnchorComponent.with(
+            HistoryTokenAnchorComponent.empty(),
+            this::getter,
+            this::setter
+        );
+        this.setId(id);
+        this.context = Objects.requireNonNull(context, "context");
+    }
+
+    /**
+     * Getter that returns the {@link SpreadsheetExpressionReference} if one is present.
+     */
+    private Optional<SpreadsheetExpressionReference> getter(final HistoryTokenAnchorComponent anchor) {
+        return anchor.historyToken()
+            .map(t -> t.cast(SpreadsheetCellSelectHistoryToken.class)
+                .anchoredSelection()
+                .selection()
+                .toExpressionReference()
+            );
+    }
+
+    private void setter(final Optional<SpreadsheetExpressionReference> value,
+                        final HistoryTokenAnchorComponent anchor) {
+        String text = null;
+        HistoryToken historyToken = null;
+
+        if (value.isPresent()) {
+            historyToken = this.context.historyToken()
+                .setAnchoredSelection(
+                    value.map(SpreadsheetSelection::setDefaultAnchor)
+                ).formula();
+            if (false == (historyToken instanceof SpreadsheetCellFormulaSelectHistoryToken)) {
+                historyToken = null;
+            }
+            text = value.get()
+                .text();
+        } else {
+            text = "Formula";
+        }
+
+        anchor.setHistoryToken(
+            Optional.ofNullable(historyToken)
+        ).setTextContent(text);
+    }
+
+    @Override
+    public Optional<SpreadsheetExpressionReference> value() {
+        return this.component.value();
+    }
+
+    @Override
+    public SpreadsheetFormulaSelectAnchorComponent setValue(final Optional<SpreadsheetExpressionReference> value) {
+        this.component.setValue(value);
+        return this;
+    }
+
+    // AnchorComponentDelegator.........................................................................................
+
+    @Override
+    public AnchorComponent<?, SpreadsheetExpressionReference> anchorComponent() {
+        return this.component;
+    }
+
+    // @VisibleForTesting
+    final ValueHistoryTokenAnchorComponent<SpreadsheetExpressionReference> component;
+
+    final HistoryTokenContext context;
+
+    // toString.........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.component.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/formula/SpreadsheetFormulaSelectAnchorComponentTest.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.formula;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.anchor.AnchorComponentTesting;
+import walkingkooka.spreadsheet.dominokit.history.FakeHistoryTokenContext;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenContexts;
+import walkingkooka.spreadsheet.reference.SpreadsheetExpressionReference;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetFormulaSelectAnchorComponentTest implements AnchorComponentTesting<SpreadsheetFormulaSelectAnchorComponent, SpreadsheetExpressionReference> {
+
+    @Test
+    public void testWithNullContextFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> SpreadsheetFormulaSelectAnchorComponent.with(
+                "id123",
+                null
+            )
+        );
+    }
+
+    // value............................................................................................................
+
+    @Test
+    public void testClearValue() {
+        this.treePrintAndCheck(
+            SpreadsheetFormulaSelectAnchorComponent.with(
+                "formula-anchor-id",
+                HistoryTokenContexts.fake()
+            ).clearValue(),
+            "\"Formula\" DISABLED id=formula-anchor-id"
+        );
+    }
+
+    @Test
+    public void testSetValueWithCellMissingSpreadsheetIdGivesDisabledLink() {
+        this.treePrintAndCheck(
+            this.createComponent(
+                "/"
+            ).setValue(
+                Optional.of(
+                    SpreadsheetSelection.A1
+                )
+            ),
+            "\"A1\" DISABLED id=formula-anchor-id"
+        );
+    }
+
+    @Test
+    public void testSetValueWithCell() {
+        this.treePrintAndCheck(
+            this.createComponent()
+                .setValue(
+                    Optional.of(
+                        SpreadsheetSelection.A1
+                    )
+                ),
+            "\"A1\" [#/1/SpreadsheetName22/cell/A1/formula] id=formula-anchor-id"
+        );
+    }
+
+    @Test
+    public void testSetValueWithCellRange() {
+        this.treePrintAndCheck(
+            this.createComponent()
+                .setValue(
+                    Optional.of(
+                        SpreadsheetSelection.parseCellRange("B2:C3")
+                    )
+                ),
+            "\"B2:C3\" [#/1/SpreadsheetName22/cell/B2:C3/bottom-right/formula] id=formula-anchor-id"
+        );
+    }
+
+    @Test
+    public void testSetValueWithLabel() {
+        this.treePrintAndCheck(
+            this.createComponent()
+                .setValue(
+                    Optional.of(
+                        SpreadsheetSelection.labelName("Label9999")
+                    )
+                ),
+            "\"Label9999\" [#/1/SpreadsheetName22/cell/Label9999/formula] id=formula-anchor-id"
+        );
+    }
+
+    @Override
+    public SpreadsheetFormulaSelectAnchorComponent createComponent() {
+        return this.createComponent("/1/SpreadsheetName22");
+    }
+
+    private SpreadsheetFormulaSelectAnchorComponent createComponent(final String currentHistoryToken) {
+        return SpreadsheetFormulaSelectAnchorComponent.with(
+            "formula-anchor-id",
+            new FakeHistoryTokenContext() {
+                @Override
+                public HistoryToken historyToken() {
+                    return HistoryToken.parseString(currentHistoryToken);
+                }
+
+                @Override
+                public String toString() {
+                    return currentHistoryToken;
+                }
+            }
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<SpreadsheetFormulaSelectAnchorComponent> type() {
+        return SpreadsheetFormulaSelectAnchorComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
