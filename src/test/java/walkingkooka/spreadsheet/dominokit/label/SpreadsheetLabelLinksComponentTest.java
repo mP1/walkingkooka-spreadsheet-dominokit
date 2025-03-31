5be3a828bf90d9c8aa7dc299f654124c975f8b98
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-03-31 22:53:38 +1100

    SpreadsheetLabelLinksComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/4766
    - SpreadsheetLabelMappingLinksComponent: delete, cell references

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/label/SpreadsheetLabelLinksComponentTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.label;
+
+import elemental2.dom.HTMLDivElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.set.Sets;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.SpreadsheetId;
+import walkingkooka.spreadsheet.SpreadsheetName;
+import walkingkooka.spreadsheet.dominokit.HtmlElementComponentTesting;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.reference.SpreadsheetExpressionReference;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+
+import java.util.Optional;
+import java.util.Set;
+
+public final class SpreadsheetLabelLinksComponentTest implements HtmlElementComponentTesting<SpreadsheetLabelLinksComponent, HTMLDivElement> {
+
+    @Test
+    public void testClearValue() {
+        this.treePrintAndCheck(
+            SpreadsheetLabelLinksComponent.empty(
+                "labels-",
+                new FakeSpreadsheetLabelLinksComponentContext() {
+
+                    @Override
+                    public HistoryToken historyToken() {
+                        return HistoryToken.cellFormula(
+                            SpreadsheetId.with(1),
+                            SpreadsheetName.with("SpreadsheetName222"),
+                            SpreadsheetSelection.A1.setDefaultAnchor()
+                        );
+                    }
+                }
+            ),
+            "SpreadsheetLabelLinksComponent\n" +
+                "  SpreadsheetLinkListComponent\n" +
+                "    SpreadsheetFlexLayout\n" +
+                "      ROW\n" +
+                "        \"References\" DISABLED id=labels-references-Link\n" +
+                "        \"Delete\" DISABLED id=labels-delete-Link\n"
+        );
+    }
+
+    @Test
+    public void testSetValue() {
+        this.treePrintAndCheck(
+            SpreadsheetLabelLinksComponent.empty(
+                "labels-",
+                new FakeSpreadsheetLabelLinksComponentContext() {
+
+                    @Override
+                    public HistoryToken historyToken() {
+                        return HistoryToken.cellFormula(
+                            SpreadsheetId.with(1),
+                            SpreadsheetName.with("SpreadsheetName222"),
+                            SpreadsheetSelection.A1.setDefaultAnchor()
+                        );
+                    }
+
+                    @Override
+                    public Set<SpreadsheetExpressionReference> cellReferences(final SpreadsheetExpressionReference spreadsheetExpressionReference) {
+                        return Sets.of(
+                            SpreadsheetSelection.parseCell("B2"),
+                            SpreadsheetSelection.parseCell("C3")
+                        );
+                    }
+                }
+            ).setValue(
+                Optional.of(
+                    SpreadsheetSelection.labelName("LABELZZZ")
+                )
+            ),
+            "SpreadsheetLabelLinksComponent\n" +
+                "  SpreadsheetLinkListComponent\n" +
+                "    SpreadsheetFlexLayout\n" +
+                "      ROW\n" +
+                "        \"References\" [#/1/SpreadsheetName222/cell/LABELZZZ/references] (2) id=labels-references-Link\n" +
+                "        \"Delete\" [#/1/SpreadsheetName222/label/LABELZZZ/delete] id=labels-delete-Link\n"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<SpreadsheetLabelLinksComponent> type() {
+        return SpreadsheetLabelLinksComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
