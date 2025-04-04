cdaa4b52146797b95bc00a67345f2ce9925b36d0
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-04-04 20:02:29 +1100

    ValidatorNameComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/validation/ValidatorNameComponent.java
@@ -0,0 +1,57 @@
+
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.validation;
+
+
+import walkingkooka.spreadsheet.dominokit.value.ValueSpreadsheetTextBox;
+import walkingkooka.spreadsheet.dominokit.value.ValueSpreadsheetTextBoxWrapper;
+import walkingkooka.validation.provider.ValidatorName;
+
+/**
+ * A text box that accepts entry and validates it as a {@link ValidatorName}.
+ */
+public final class ValidatorNameComponent implements ValueSpreadsheetTextBoxWrapper<ValidatorNameComponent, ValidatorName> {
+
+    public static ValidatorNameComponent empty() {
+        return new ValidatorNameComponent();
+    }
+
+    private ValidatorNameComponent() {
+        this.textBox = ValueSpreadsheetTextBox.with(
+            ValidatorName::with,
+            ValidatorName::toString
+        );
+    }
+
+    // ValueSpreadsheetTextBoxWrapper..................................................................................
+
+    @Override
+    public ValueSpreadsheetTextBox<ValidatorName> parserSpreadsheetTextBox() {
+        return this.textBox;
+    }
+
+    private final ValueSpreadsheetTextBox<ValidatorName> textBox;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.textBox.toString();
+    }
+}
