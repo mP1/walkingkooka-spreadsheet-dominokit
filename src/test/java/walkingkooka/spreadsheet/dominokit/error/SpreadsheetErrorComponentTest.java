2929c7f3b487aa86937c977428b35f709ce7b4bf
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-05-25 15:48:04 +1000

    SpreadsheetErrorComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/4851
    - SpreadsheetErrorComponent

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/error/SpreadsheetErrorComponentTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.error;
+
+import elemental2.dom.HTMLFieldSetElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.SpreadsheetError;
+import walkingkooka.spreadsheet.SpreadsheetErrorKind;
+import walkingkooka.spreadsheet.dominokit.value.FormValueComponentTesting;
+
+import java.util.Optional;
+
+public final class SpreadsheetErrorComponentTest implements FormValueComponentTesting<HTMLFieldSetElement, SpreadsheetError, SpreadsheetErrorComponent> {
+
+    private final static SpreadsheetError ERROR = SpreadsheetErrorKind.DIV0.setMessage("Hello Error 123");
+
+    @Test
+    public void testParseAndText() {
+        this.checkEquals(
+            ERROR,
+            SpreadsheetError.parse(
+                ERROR.textIncludingMessage()
+            )
+        );
+    }
+
+    @Test
+    public void testSetStringValueAndValue() {
+        this.checkEquals(
+            Optional.of(ERROR),
+            SpreadsheetErrorComponent.empty()
+                .setStringValue(
+                    Optional.of(
+                        ERROR.textIncludingMessage()
+                    )
+                ).value()
+        );
+    }
+
+    @Test
+    public void testSetStringValue() {
+        this.treePrintAndCheck(
+            SpreadsheetErrorComponent.empty()
+                .setStringValue(
+                    Optional.of(
+                        ERROR.textIncludingMessage()
+                    )
+                ),
+            "SpreadsheetErrorComponent\n" +
+                "  ValueSpreadsheetTextBox\n" +
+                "    SpreadsheetTextBox\n" +
+                "      [#DIV/0! Hello Error 123]\n"
+        );
+    }
+
+    @Test
+    public void testSetStringValueWithInvalid() {
+        this.treePrintAndCheck(
+            SpreadsheetErrorComponent.empty()
+                .setStringValue(
+                    Optional.of(
+                        "#BAD Message123"
+                    )
+                ),
+            "SpreadsheetErrorComponent\n" +
+                "  ValueSpreadsheetTextBox\n" +
+                "    SpreadsheetTextBox\n" +
+                "      [#BAD Message123]\n" +
+                "      Errors\n" +
+                "        Invalid error kind\n"
+        );
+    }
+
+    // ValueComponent...................................................................................................
+
+    @Override
+    public SpreadsheetErrorComponent createComponent() {
+        return SpreadsheetErrorComponent.empty();
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<SpreadsheetErrorComponent> type() {
+        return SpreadsheetErrorComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
