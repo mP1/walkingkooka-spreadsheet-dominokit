6da37f8ae6b07bb36f92e959e93468888b7dcb6c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-05-27 17:51:59 +1000

    HistoryToken.freeze refactoring

--- src/main/java/walkingkooka/spreadsheet/dominokit/history/HistoryToken.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/history/HistoryToken.java
@@ -2405,32 +2405,33 @@ public abstract class HistoryToken implements HasUrlFragment,
     public final HistoryToken freeze() {
         HistoryToken historyToken = this;
 
-        if (this instanceof SpreadsheetCellHistoryToken) {
-            final SpreadsheetCellHistoryToken cell = this.cast(SpreadsheetCellHistoryToken.class);
+        if (this instanceof SpreadsheetAnchoredSelectionHistoryToken) {
+            final SpreadsheetAnchoredSelectionHistoryToken anchored = this.cast(SpreadsheetAnchoredSelectionHistoryToken.class);
+            final SpreadsheetId id = anchored.id();
+            final SpreadsheetName name = anchored.name();
+            final AnchoredSpreadsheetSelection anchoredSpreadsheetSelection = anchored.anchoredSelection();
 
-            historyToken = cellFreeze(
-                cell.id(),
-                cell.name(),
-                cell.anchoredSelection()
-            );
-        } else {
-            if (this instanceof SpreadsheetColumnHistoryToken) {
-                final SpreadsheetColumnHistoryToken column = this.cast(SpreadsheetColumnHistoryToken.class);
-
-                historyToken = columnFreeze(
-                    column.id(),
-                    column.name(),
-                    column.anchoredSelection()
+            if (this instanceof SpreadsheetCellHistoryToken) {
+                historyToken = cellFreeze(
+                    id,
+                    name,
+                    anchoredSpreadsheetSelection
                 );
             } else {
-                if (this instanceof SpreadsheetRowHistoryToken) {
-                    final SpreadsheetRowHistoryToken row = this.cast(SpreadsheetRowHistoryToken.class);
-
-                    historyToken = rowFreeze(
-                        row.id(),
-                        row.name(),
-                        row.anchoredSelection()
+                if (this instanceof SpreadsheetColumnHistoryToken) {
+                    historyToken = columnFreeze(
+                        id,
+                        name,
+                        anchoredSpreadsheetSelection
                     );
+                } else {
+                    if (this instanceof SpreadsheetRowHistoryToken) {
+                        historyToken = rowFreeze(
+                            id,
+                            name,
+                            anchoredSpreadsheetSelection
+                        );
+                    }
                 }
             }
         }
