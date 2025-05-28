9b30451bb7b46bca8bc7262dc0468eff4f06e35f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-05-28 18:49:01 +1000

    DecimalNumberSymbolsDialogComponentContext

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/decimalnumbersymbols/AppContextSpreadsheetMetadataPropertyDecimalNumberSymbolsDialogComponentContext.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.decimalnumbersymbols;
+
+import walkingkooka.math.DecimalNumberSymbols;
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.spreadsheet.dominokit.history.HistoryContext;
+import walkingkooka.spreadsheet.dominokit.history.HistoryContextDelegator;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.SpreadsheetMetadataPropertySaveHistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.SpreadsheetMetadataPropertySelectHistoryToken;
+import walkingkooka.spreadsheet.dominokit.log.LoggingContext;
+import walkingkooka.spreadsheet.dominokit.log.LoggingContextDelegator;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class AppContextSpreadsheetMetadataPropertyDecimalNumberSymbolsDialogComponentContext implements DecimalNumberSymbolsDialogComponentContext,
+    HistoryContextDelegator,
+    LoggingContextDelegator {
+
+    static AppContextSpreadsheetMetadataPropertyDecimalNumberSymbolsDialogComponentContext with(final AppContext context) {
+        return new AppContextSpreadsheetMetadataPropertyDecimalNumberSymbolsDialogComponentContext(
+            Objects.requireNonNull(context, "context")
+        );
+    }
+
+    private AppContextSpreadsheetMetadataPropertyDecimalNumberSymbolsDialogComponentContext(final AppContext context) {
+        super();
+        this.context = context;
+    }
+
+    @Override
+    public String dialogTitle() {
+        return DecimalNumberSymbols.class.getSimpleName();
+    }
+
+    @Override
+    public Optional<DecimalNumberSymbols> loadDecimalNumberSymbols() {
+        return this.context.spreadsheetMetadata()
+            .get(SpreadsheetMetadataPropertyName.DECIMAL_NUMBER_SYMBOLS);
+    }
+
+    @Override
+    public void save(final Optional<DecimalNumberSymbols> symbols) {
+        final AppContext context = this.context;
+        context.spreadsheetMetadataFetcher()
+            .patchMetadata(
+                context.spreadsheetMetadata()
+                    .getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID),
+                SpreadsheetMetadata.EMPTY.setOrRemove(
+                    SpreadsheetMetadataPropertyName.DECIMAL_NUMBER_SYMBOLS,
+                    symbols.orElse(null)
+                )
+            );
+    }
+
+    @Override
+    public boolean shouldIgnore(final HistoryToken token) {
+        return token instanceof SpreadsheetMetadataPropertySaveHistoryToken;
+    }
+
+    @Override
+    public boolean isMatch(final HistoryToken token) {
+        return token instanceof SpreadsheetMetadataPropertySelectHistoryToken &&
+            token.cast(SpreadsheetMetadataPropertySelectHistoryToken.class).propertyName() == SpreadsheetMetadataPropertyName.DECIMAL_NUMBER_SYMBOLS;
+    }
+
+    @Override
+    public HistoryContext historyContext() {
+        return this.context;
+    }
+
+    @Override
+    public LoggingContext loggingContext() {
+        return this.context;
+    }
+
+    private final AppContext context;
+
+    @Override
+    public String toString() {
+        return this.context.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/decimalnumbersymbols/AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContextTest.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.decimalnumbersymbols;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.set.Sets;
+import walkingkooka.math.DecimalNumberSymbols;
+import walkingkooka.net.Url;
+import walkingkooka.net.http.HttpMethod;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.spreadsheet.dominokit.FakeAppContext;
+import walkingkooka.spreadsheet.dominokit.fetcher.SpreadsheetDeltaFetcherWatcher;
+import walkingkooka.spreadsheet.dominokit.fetcher.SpreadsheetMetadataFetcherWatcher;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenWatcher;
+import walkingkooka.spreadsheet.dominokit.viewport.SpreadsheetViewportCache;
+import walkingkooka.spreadsheet.engine.SpreadsheetDelta;
+import walkingkooka.spreadsheet.formula.SpreadsheetFormula;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataTesting;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+
+import java.util.Optional;
+
+public final class AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContextTest implements DecimalNumberSymbolsDialogComponentContextTesting<AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContext>,
+    SpreadsheetMetadataTesting {
+
+    @Test
+    public void testIsMatchWhenSpreadsheetCellDecimalNumberSymbolsSaveHistoryToken() {
+        this.isMatchAndCheck(
+            this.createContext(),
+            HistoryToken.cellDecimalNumberSymbolsSave(
+                SPREADSHEET_ID,
+                SPREADSHEET_NAME,
+                SpreadsheetSelection.A1.setDefaultAnchor(),
+                Optional.empty()
+            ),
+            false
+        );
+    }
+
+    @Test
+    public void testIsMatchWhenSpreadsheetCellDecimalNumberSymbolsSelectHistoryToken() {
+        this.isMatchAndCheck(
+            this.createContext(),
+            HistoryToken.cellDecimalNumberSymbolsSelect(
+                SPREADSHEET_ID,
+                SPREADSHEET_NAME,
+                SpreadsheetSelection.A1.setDefaultAnchor()
+            ),
+            true
+        );
+    }
+
+    @Test
+    public void testIsMatchWhenSpreadsheetCellSelectHistoryToken() {
+        this.isMatchAndCheck(
+            this.createContext(),
+            HistoryToken.cellSelect(
+                SPREADSHEET_ID,
+                SPREADSHEET_NAME,
+                SpreadsheetSelection.A1.setDefaultAnchor()
+            ),
+            false
+        );
+    }
+
+    @Test
+    public void testIsMatchWhenSpreadsheetMetadataPropertySelectHistoryTokenDecimalNumberSymbols() {
+        this.isMatchAndCheck(
+            this.createContext(),
+            HistoryToken.metadataPropertySelect(
+                SPREADSHEET_ID,
+                SPREADSHEET_NAME,
+                SpreadsheetMetadataPropertyName.DECIMAL_NUMBER_SYMBOLS
+            ),
+            false
+        );
+    }
+
+    @Test
+    public void testIsMatchWhenSpreadsheetMetadataPropertySelectHistoryTokenDateTimeSymbols() {
+        this.isMatchAndCheck(
+            this.createContext(),
+            HistoryToken.metadataPropertySelect(
+                SPREADSHEET_ID,
+                SPREADSHEET_NAME,
+                SpreadsheetMetadataPropertyName.DATE_TIME_SYMBOLS
+            ),
+            false
+        );
+    }
+
+    // load.............................................................................................................
+
+    @Test
+    public void testLoadDecimalNumberSymbols() {
+        final Optional<DecimalNumberSymbols> decimalNumberSymbols = Optional.of(DECIMAL_NUMBER_SYMBOLS);
+
+        final SpreadsheetCell cell = SpreadsheetSelection.A1.setFormula(SpreadsheetFormula.EMPTY)
+            .setDecimalNumberSymbols(decimalNumberSymbols);
+
+        final AppContext appContext = new FakeAppContext() {
+            @Override
+            public SpreadsheetViewportCache spreadsheetViewportCache() {
+                return cache;
+            }
+
+            final SpreadsheetViewportCache cache = SpreadsheetViewportCache.empty(this);
+
+            @Override
+            public Runnable addHistoryTokenWatcher(final HistoryTokenWatcher watcher) {
+                return null;
+            }
+
+            @Override
+            public Runnable addSpreadsheetDeltaFetcherWatcher(final SpreadsheetDeltaFetcherWatcher watcher) {
+                return null;
+            }
+
+            @Override
+            public Runnable addSpreadsheetMetadataFetcherWatcher(final SpreadsheetMetadataFetcherWatcher watcher) {
+                return null;
+            }
+
+            @Override
+            public HistoryToken historyToken() {
+                return HistoryToken.cellDecimalNumberSymbolsSelect(
+                    SPREADSHEET_ID,
+                    SPREADSHEET_NAME,
+                    SpreadsheetSelection.A1.setDefaultAnchor()
+                );
+            }
+
+            @Override
+            public void debug(final Object... values) {
+                // NOP
+            }
+        };
+
+        appContext.spreadsheetViewportCache()
+                .onSpreadsheetMetadata(
+                    METADATA_EN_AU.set(SpreadsheetMetadataPropertyName.SPREADSHEET_ID, SPREADSHEET_ID),
+                    appContext
+                );
+
+        appContext.spreadsheetViewportCache()
+            .onSpreadsheetDelta(
+                HttpMethod.GET,
+                Url.parseRelative("/api/spreadsheet/1/cell/*"),
+                SpreadsheetDelta.EMPTY.setCells(
+                    Sets.of(
+                        cell
+                    )
+                ),
+                appContext
+            );
+
+        this.loadDecimalNumberSymbolsAndCheck(
+            AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContext.with(
+                appContext
+            ),
+            decimalNumberSymbols
+        );
+    }
+
+    @Override
+    public AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContext createContext() {
+        return AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContext.with(
+            new FakeAppContext() {
+
+            }
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContext> type() {
+        return AppContextSpreadsheetCellDecimalNumberSymbolsDialogComponentContext.class;
+    }
+}
