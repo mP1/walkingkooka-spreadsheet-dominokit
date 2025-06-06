3aaed5e1df0492d057f8cad140c8d9718a60a7bf
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-06 14:06:56 +1000

    SpreadsheetDataComponent

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/SpreadsheetDateComponent.java
@@ -0,0 +1,258 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value;
+
+import elemental2.dom.EventListener;
+import elemental2.dom.HTMLFieldSetElement;
+import org.dominokit.domino.ui.utils.HasChangeListeners.ChangeListener;
+import org.dominokit.domino.ui.utils.HasValidation.Validator;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.spreadsheet.dominokit.TestHtmlElementComponent;
+import walkingkooka.spreadsheet.dominokit.ValidatorHelper;
+import walkingkooka.text.CharSequences;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.TreePrintable;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * A mock of main/SpreadsheetTextBox with the same public interface and a helpful {@link TreePrintable}. This will be useful for unit tests to verify the rough apperance of a component that includes
+ * {@link SpreadsheetDateComponent}.
+ */
+public final class SpreadsheetDateComponent implements FormValueComponent<HTMLFieldSetElement, LocalDate, SpreadsheetDateComponent>,
+    SpreadsheetTextBoxTreePrintable<SpreadsheetDateComponent, LocalDate>,
+    TestHtmlElementComponent<HTMLFieldSetElement, SpreadsheetDateComponent>,
+    ValidatorHelper {
+
+    public static SpreadsheetDateComponent empty(final String id) {
+        return new SpreadsheetDateComponent(id);
+    }
+
+    private SpreadsheetDateComponent(final String id) {
+        this.setId(id);
+    }
+
+    @Override
+    public SpreadsheetDateComponent setId(final String id) {
+        CharSequences.failIfNullOrEmpty(id, "id");
+
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public String id() {
+        return this.id;
+    }
+
+    private String id;
+
+    @Override
+    public SpreadsheetDateComponent setLabel(final String label) {
+        this.label = label;
+        return this;
+    }
+
+    @Override
+    public String label() {
+        return this.label;
+    }
+
+    private String label;
+
+    @Override
+    public SpreadsheetDateComponent setValue(final Optional<LocalDate> value) {
+        Objects.requireNonNull(value, "value")
+            .orElseThrow(() -> new IllegalArgumentException("Date value required"));
+        this.value = value;
+
+        return validate();
+    }
+
+    @Override
+    public Optional<LocalDate> value() {
+        return this.value;
+    }
+
+    private Optional<LocalDate> value = Optional.empty();
+
+    @Override
+    public SpreadsheetDateComponent focus() {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent optional() {
+        this.required = false;
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent required() {
+        this.required = true;
+        return this;
+    }
+
+    @Override
+    public boolean isRequired() {
+        return this.required;
+    }
+
+    private boolean required;
+
+    public SpreadsheetDateComponent setValidator(final Validator<Optional<LocalDate>> validator) {
+        this.validator = validator;
+        return this;
+    }
+
+    private Validator<Optional<LocalDate>> validator;
+
+    @Override
+    public SpreadsheetDateComponent validate() {
+        this.setErrors(
+            this.validateAndGetErrors(
+                this.value,
+                Optional.ofNullable(this.validator)
+            )
+        );
+        return this;
+    }
+
+    @Override
+    public List<String> errors() {
+        return this.errors;
+    }
+
+    @Override
+    public SpreadsheetDateComponent setErrors(final List<String> errors) {
+        Objects.requireNonNull(errors, "errors");
+        this.errors = Lists.immutable(errors);
+        return this;
+    }
+
+    private List<String> errors = Lists.empty();
+
+    @Override
+    public boolean isDisabled() {
+        return this.disabled;
+    }
+
+    @Override
+    public SpreadsheetDateComponent setDisabled(final boolean disabled) {
+        this.disabled = disabled;
+        return this;
+    }
+
+    private boolean disabled;
+
+    public SpreadsheetDateComponent resetView() {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent addChangeListener(final ChangeListener<Optional<LocalDate>> listener) {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent addClickListener(final EventListener listener) {
+        return this;
+    }
+    
+    @Override
+    public SpreadsheetDateComponent addFocusListener(final EventListener listener) {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent addKeydownListener(final EventListener listener) {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent addKeyupListener(final EventListener listener) {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent alwaysShowHelperText() {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent setHelperText(final Optional<String> text) {
+        Objects.requireNonNull(text, "text");
+        this.helperText = text;
+        return this;
+    }
+
+    @Override public Optional<String> helperText() {
+        return this.helperText;
+    }
+
+    private Optional<String> helperText = Optional.empty();
+
+    @Override
+    public SpreadsheetDateComponent hideMarginBottom() {
+        return this;
+    }
+
+    @Override
+    public SpreadsheetDateComponent removeBorders() {
+        return this;
+    }
+
+    public SpreadsheetDateComponent autocompleteOff() {
+        return this;
+    }
+
+    public SpreadsheetDateComponent clearIcon() {
+        return this;
+    }
+
+    public SpreadsheetDateComponent disableSpellcheck() {
+        return this;
+    }
+
+    public SpreadsheetDateComponent enterFiresValueChange() {
+        return this;
+    }
+
+    public SpreadsheetDateComponent magnifyingGlassIcon() {
+        return this;
+    }
+
+    // FIXES
+    //
+    // java.lang.NoSuchMethodError: walkingkooka.spreadsheet.dominokit.value.SpreadsheetTextBox.setCssText(Ljava/lang/String;)Lwalkingkooka/spreadsheet/dominokit/ui/textbox/SpreadsheetTextBox;
+    @Override
+    public SpreadsheetDateComponent setCssText(final String css) {
+        Objects.requireNonNull(css, "css");
+        return this;
+    }
+
+    // SpreadsheetTextBoxTreePrintable..................................................................................
+
+    @Override
+    public void treePrintAlternateValues(final IndentingPrinter printer) {
+        // NOP
+    }
+}
