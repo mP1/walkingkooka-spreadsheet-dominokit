c156f924491f30cf8476fb0b258f72a5aaa7ab81
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-07-11 14:50:32 +1000

    AppContextSpreadsheetLocaleComponentDialogComponentContext was BasicSpreadsheetLocaleComponentDialogComponentContext

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/locale/AppContextSpreadsheetLocaleComponentDialogComponentContext.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.locale;
+
+import walkingkooka.locale.LocaleContext;
+import walkingkooka.locale.LocaleContextDelegator;
+import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.spreadsheet.dominokit.dialog.SpreadsheetDialogComponentContext;
+import walkingkooka.spreadsheet.dominokit.dialog.SpreadsheetDialogComponentContextDelegator;
+
+import java.util.Objects;
+
+final class AppContextSpreadsheetLocaleComponentDialogComponentContext implements SpreadsheetLocaleComponentDialogComponentContext,
+    SpreadsheetDialogComponentContextDelegator,
+    LocaleContextDelegator {
+
+    static AppContextSpreadsheetLocaleComponentDialogComponentContext with(final AppContext context) {
+        return new AppContextSpreadsheetLocaleComponentDialogComponentContext(
+            Objects.requireNonNull(context, "context")
+        );
+    }
+
+    private AppContextSpreadsheetLocaleComponentDialogComponentContext(final AppContext context) {
+        this.context = context;
+    }
+
+    // SpreadsheetDialogComponentContext................................................................................
+
+    @Override
+    public SpreadsheetDialogComponentContext spreadsheetDialogComponentContext() {
+        return this.context;
+    }
+
+    // LocaleContextDelegator...........................................................................................
+
+    @Override
+    public LocaleContext localeContext() {
+        return this.context;
+    }
+
+    private final AppContext context;
+}
