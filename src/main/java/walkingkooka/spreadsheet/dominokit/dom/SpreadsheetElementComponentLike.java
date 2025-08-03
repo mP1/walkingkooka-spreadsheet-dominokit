ae06b5a55b00b4b64a7a57615fadc9b950073499
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-08-03 15:01:07 +1000

    SpreadsheetElementComponent.addXXXListener

--- src/main/java/walkingkooka/spreadsheet/dominokit/dom/SpreadsheetElementComponentLike.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/dom/SpreadsheetElementComponentLike.java
@@ -21,6 +21,7 @@ import elemental2.dom.EventListener;
 import elemental2.dom.HTMLElement;
 import elemental2.dom.Node;
 import org.dominokit.domino.ui.IsElement;
+import org.dominokit.domino.ui.events.EventType;
 import walkingkooka.spreadsheet.dominokit.HtmlElementComponent;
 
 /**
@@ -104,6 +105,104 @@ abstract class SpreadsheetElementComponentLike<E extends HTMLElement, C extends 
     public abstract C appendChild(final Node child);
 
     public abstract C appendChild(final IsElement<?> child);
+
+    public final C addBlurListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.blur.getName(),
+            listener
+        );
+    }
+
+    public final C addChangeListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.change.getName(),
+            listener
+        );
+    }
+
+    public final C addClickListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.click.getName(),
+            listener
+        );
+    }
+
+    public final C addDoubleClickListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.dblclick.getName(),
+            listener
+        );
+    }
+
+    public final C addFocusListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.focus.getName(),
+            listener
+        );
+    }
+
+    public final C addKeyDownListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.keydown.getName(),
+            listener
+        );
+    }
+
+    public final C addKeyPressListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.keypress.getName(),
+            listener
+        );
+    }
+
+    public final C addKeyUpListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.keyup.getName(),
+            listener
+        );
+    }
+
+    public final C addMouseDownListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.mousedown.getName(),
+            listener
+        );
+    }
+
+    public final C addMouseEnterListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.mouseenter.getName(),
+            listener
+        );
+    }
+
+    public final C addMouseLeaveListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.mouseleave.getName(),
+            listener
+        );
+    }
+
+    public final C addMouseOverListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.mouseup.getName(),
+            listener
+        );
+    }
+
+    public final C addMouseOutListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.mouseout.getName(),
+            listener
+        );
+    }
+
+    public final C addMouseUpListener(final EventListener listener) {
+        return this.addEventListener(
+            EventType.mouseup.getName(),
+            listener
+        );
+    }
 
     public abstract C addEventListener(final String type,
                                        final EventListener listener);
