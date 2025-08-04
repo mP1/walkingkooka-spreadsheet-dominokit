7b38fccf722f8662243c04c8f224d015f14712e1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-08-04 18:59:51 +1000

    HasEventListenersDelegator
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/5840
    - HasEventListenersDelegator

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/dom/HasEventListenersDelegator.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.dom;
+
+import elemental2.dom.EventListener;
+import org.dominokit.domino.ui.utils.HasChangeListeners.ChangeListener;
+
+import java.util.Optional;
+
+public interface HasEventListenersDelegator<V, C extends HasEventListeners<V, C>> extends HasEventListeners<V, C> {
+
+    @Override
+    default C addChangeListener(final ChangeListener<Optional<V>> listener) {
+        return this.hasEventListeners()
+            .addChangeListener(listener);
+    }
+
+    @Override
+    default C addClickListener(final EventListener listener) {
+        return this.hasEventListeners()
+            .addClickListener(listener);
+    }
+
+    @Override
+    default C addContextMenuListener(final EventListener listener) {
+        return this.hasEventListeners()
+            .addContextMenuListener(listener);
+    }
+
+    @Override
+    default C addFocusListener(final EventListener listener) {
+        return this.hasEventListeners()
+            .addFocusListener(listener);
+    }
+
+    @Override
+    default C addKeydownListener(final EventListener listener) {
+        return this.hasEventListeners()
+            .addKeydownListener(listener);
+    }
+
+    @Override
+    default C addKeyupListener(final EventListener listener) {
+        return this.hasEventListeners()
+            .addKeyupListener(listener);
+    }
+
+    C hasEventListeners();
+}
