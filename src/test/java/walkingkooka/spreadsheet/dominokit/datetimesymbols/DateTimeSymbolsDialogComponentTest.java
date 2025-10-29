776838af2277d9c035531b1be1ec9ad2729170f6
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-10-29 13:21:33 +1100

    DateTimeSymbolsDialogComponent.id= "DateTimeSymbols" was "dateTimeSymbols"

--- src/test/java/walkingkooka/spreadsheet/dominokit/datetimesymbols/DateTimeSymbolsDialogComponentTest.java
+++ src/test/java/walkingkooka/spreadsheet/dominokit/datetimesymbols/DateTimeSymbolsDialogComponentTest.java
@@ -65,37 +65,37 @@ public final class DateTimeSymbolsDialogComponentTest implements DialogComponent
             "DateTimeSymbolsDialogComponent\n" +
                 "  DialogComponent\n" +
                 "    A1: Date Time Symbols\n" +
-                "    id=dateTimeSymbols-Dialog includeClose=true\n" +
+                "    id=DateTimeSymbols-Dialog includeClose=true\n" +
                 "      FlexLayoutComponent\n" +
                 "        ROW\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                AM/PM [] id=dateTimeSymbolsampms-TextBox\n" +
+                "                AM/PM [] id=DateTimeSymbols-ampms-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 2\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Month names [] id=dateTimeSymbolsmonthNames-TextBox\n" +
+                "                Month names [] id=DateTimeSymbols-monthNames-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 12 or more\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Month name abbreviations [] id=dateTimeSymbolsmonthNameAbbreviations-TextBox\n" +
+                "                Month name abbreviations [] id=DateTimeSymbols-monthNameAbbreviations-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 12 or more\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Week day names [] id=dateTimeSymbolsweekDayNames-TextBox\n" +
+                "                Week day names [] id=DateTimeSymbols-weekDayNames-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 7\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Week day names Abbreviations [] id=dateTimeSymbolsweekDayNameAbbreviations-TextBox\n" +
+                "                Week day names Abbreviations [] id=DateTimeSymbols-weekDayNameAbbreviations-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 7\n" +
                 "          DateTimeSymbolsComponent\n" +
@@ -110,11 +110,11 @@ public final class DateTimeSymbolsDialogComponentTest implements DialogComponent
                 "      AnchorListComponent\n" +
                 "        FlexLayoutComponent\n" +
                 "          ROW\n" +
-                "            \"Save\" DISABLED id=dateTimeSymbols-save-Link\n" +
-                "            \"Clear\" [#/1/SpreadsheetName1/cell/A1/dateTimeSymbols/save/] id=dateTimeSymbols-clear-Link\n" +
-                "            \"Undo\" [#/1/SpreadsheetName1/cell/A1/dateTimeSymbols/save/] id=dateTimeSymbols-undo-Link\n" +
-                "            \"Copy Defaults\" [#/1/SpreadsheetName1/cell/A1/dateTimeSymbols/save/%22am,pm%22,%22January,February,March,April,May,June,July,August,September,October,November,December%22,%22Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.%22,%22Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday%22,%22Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.%22] id=dateTimeSymbols-copyDefaults-Link\n" +
-                "            \"Close\" [#/1/SpreadsheetName1/cell/A1] id=dateTimeSymbols-close-Link\n"
+                "            \"Save\" DISABLED id=DateTimeSymbols-save-Link\n" +
+                "            \"Clear\" [#/1/SpreadsheetName1/cell/A1/dateTimeSymbols/save/] id=DateTimeSymbols-clear-Link\n" +
+                "            \"Undo\" [#/1/SpreadsheetName1/cell/A1/dateTimeSymbols/save/] id=DateTimeSymbols-undo-Link\n" +
+                "            \"Copy Defaults\" [#/1/SpreadsheetName1/cell/A1/dateTimeSymbols/save/%22am,pm%22,%22January,February,March,April,May,June,July,August,September,October,November,December%22,%22Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.%22,%22Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday%22,%22Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.%22] id=DateTimeSymbols-copyDefaults-Link\n" +
+                "            \"Close\" [#/1/SpreadsheetName1/cell/A1] id=DateTimeSymbols-close-Link\n"
         );
     }
 
@@ -142,29 +142,29 @@ public final class DateTimeSymbolsDialogComponentTest implements DialogComponent
             "DateTimeSymbolsDialogComponent\n" +
                 "  DialogComponent\n" +
                 "    Spreadsheet: Date Time Symbols (dateTimeSymbols)\n" +
-                "    id=dateTimeSymbols-Dialog includeClose=true\n" +
+                "    id=DateTimeSymbols-Dialog includeClose=true\n" +
                 "      FlexLayoutComponent\n" +
                 "        ROW\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                AM/PM [am,pm] id=dateTimeSymbolsampms-TextBox\n" +
+                "                AM/PM [am,pm] id=DateTimeSymbols-ampms-TextBox\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Month names [January,February,March,April,May,June,July,August,September,October,November,December] id=dateTimeSymbolsmonthNames-TextBox\n" +
+                "                Month names [January,February,March,April,May,June,July,August,September,October,November,December] id=DateTimeSymbols-monthNames-TextBox\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Month name abbreviations [Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.] id=dateTimeSymbolsmonthNameAbbreviations-TextBox\n" +
+                "                Month name abbreviations [Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.] id=DateTimeSymbols-monthNameAbbreviations-TextBox\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Week day names [Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday] id=dateTimeSymbolsweekDayNames-TextBox\n" +
+                "                Week day names [Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday] id=DateTimeSymbols-weekDayNames-TextBox\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Week day names Abbreviations [Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.] id=dateTimeSymbolsweekDayNameAbbreviations-TextBox\n" +
+                "                Week day names Abbreviations [Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.] id=DateTimeSymbols-weekDayNameAbbreviations-TextBox\n" +
                 "          DateTimeSymbolsComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
@@ -175,11 +175,11 @@ public final class DateTimeSymbolsDialogComponentTest implements DialogComponent
                 "      AnchorListComponent\n" +
                 "        FlexLayoutComponent\n" +
                 "          ROW\n" +
-                "            \"Save\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/%22am,pm%22,%22January,February,March,April,May,June,July,August,September,October,November,December%22,%22Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.%22,%22Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday%22,%22Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.%22] id=dateTimeSymbols-save-Link\n" +
-                "            \"Clear\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/] id=dateTimeSymbols-clear-Link\n" +
-                "            \"Undo\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/%22am,pm%22,%22January,February,March,April,May,June,July,August,September,October,November,December%22,%22Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.%22,%22Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday%22,%22Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.%22] id=dateTimeSymbols-undo-Link\n" +
-                "            \"Copy Defaults\" DISABLED id=dateTimeSymbols-copyDefaults-Link\n" +
-                "            \"Close\" [#/1/SpreadsheetName1/spreadsheet] id=dateTimeSymbols-close-Link\n"
+                "            \"Save\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/%22am,pm%22,%22January,February,March,April,May,June,July,August,September,October,November,December%22,%22Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.%22,%22Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday%22,%22Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.%22] id=DateTimeSymbols-save-Link\n" +
+                "            \"Clear\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/] id=DateTimeSymbols-clear-Link\n" +
+                "            \"Undo\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/%22am,pm%22,%22January,February,March,April,May,June,July,August,September,October,November,December%22,%22Jan.,Feb.,Mar.,Apr.,May,Jun.,Jul.,Aug.,Sep.,Oct.,Nov.,Dec.%22,%22Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday%22,%22Sun.,Mon.,Tue.,Wed.,Thu.,Fri.,Sat.%22] id=DateTimeSymbols-undo-Link\n" +
+                "            \"Copy Defaults\" DISABLED id=DateTimeSymbols-copyDefaults-Link\n" +
+                "            \"Close\" [#/1/SpreadsheetName1/spreadsheet] id=DateTimeSymbols-close-Link\n"
         );
     }
 
@@ -207,37 +207,37 @@ public final class DateTimeSymbolsDialogComponentTest implements DialogComponent
             "DateTimeSymbolsDialogComponent\n" +
                 "  DialogComponent\n" +
                 "    Spreadsheet: Date Time Symbols (dateTimeSymbols)\n" +
-                "    id=dateTimeSymbols-Dialog includeClose=true\n" +
+                "    id=DateTimeSymbols-Dialog includeClose=true\n" +
                 "      FlexLayoutComponent\n" +
                 "        ROW\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                AM/PM [] id=dateTimeSymbolsampms-TextBox\n" +
+                "                AM/PM [] id=DateTimeSymbols-ampms-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 2\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Month names [] id=dateTimeSymbolsmonthNames-TextBox\n" +
+                "                Month names [] id=DateTimeSymbols-monthNames-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 12 or more\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Month name abbreviations [] id=dateTimeSymbolsmonthNameAbbreviations-TextBox\n" +
+                "                Month name abbreviations [] id=DateTimeSymbols-monthNameAbbreviations-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 12 or more\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Week day names [] id=dateTimeSymbolsweekDayNames-TextBox\n" +
+                "                Week day names [] id=DateTimeSymbols-weekDayNames-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 7\n" +
                 "          CsvStringListComponent\n" +
                 "            ValueTextBoxComponent\n" +
                 "              TextBoxComponent\n" +
-                "                Week day names Abbreviations [] id=dateTimeSymbolsweekDayNameAbbreviations-TextBox\n" +
+                "                Week day names Abbreviations [] id=DateTimeSymbols-weekDayNameAbbreviations-TextBox\n" +
                 "                Errors\n" +
                 "                  Require 7\n" +
                 "          DateTimeSymbolsComponent\n" +
@@ -248,14 +248,15 @@ public final class DateTimeSymbolsDialogComponentTest implements DialogComponent
                 "                  Expected 5 tokens but got 0\n" +
                 "      LocaleComponent\n" +
                 "        SuggestBoxComponent\n" +
-                "          Load from Locale []\n" +"      AnchorListComponent\n" +
+                "          Load from Locale []\n" +
+                "      AnchorListComponent\n" +
                 "        FlexLayoutComponent\n" +
                 "          ROW\n" +
-                "            \"Save\" DISABLED id=dateTimeSymbols-save-Link\n" +
-                "            \"Clear\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/] id=dateTimeSymbols-clear-Link\n" +
-                "            \"Undo\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/] id=dateTimeSymbols-undo-Link\n" +
-                "            \"Copy Defaults\" DISABLED id=dateTimeSymbols-copyDefaults-Link\n" +
-                "            \"Close\" [#/1/SpreadsheetName1/spreadsheet] id=dateTimeSymbols-close-Link\n"
+                "            \"Save\" DISABLED id=DateTimeSymbols-save-Link\n" +
+                "            \"Clear\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/] id=DateTimeSymbols-clear-Link\n" +
+                "            \"Undo\" [#/1/SpreadsheetName1/spreadsheet/dateTimeSymbols/save/] id=DateTimeSymbols-undo-Link\n" +
+                "            \"Copy Defaults\" DISABLED id=DateTimeSymbols-copyDefaults-Link\n" +
+                "            \"Close\" [#/1/SpreadsheetName1/spreadsheet] id=DateTimeSymbols-close-Link\n"
         );
     }
 
