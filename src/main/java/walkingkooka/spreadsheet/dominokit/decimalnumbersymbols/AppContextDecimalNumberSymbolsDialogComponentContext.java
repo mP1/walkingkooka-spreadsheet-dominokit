576ecb0285c46b316c204e5fd74d1206318962dc
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-11-06 18:00:46 +1100

    AppContextDecimalNumberSymbolsDialogComponentContext implements RefreshContextDelegator

--- src/main/java/walkingkooka/spreadsheet/dominokit/decimalnumbersymbols/AppContextDecimalNumberSymbolsDialogComponentContext.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/decimalnumbersymbols/AppContextDecimalNumberSymbolsDialogComponentContext.java
@@ -18,17 +18,14 @@
 package walkingkooka.spreadsheet.dominokit.decimalnumbersymbols;
 
 import walkingkooka.spreadsheet.dominokit.AppContext;
+import walkingkooka.spreadsheet.dominokit.RefreshContext;
+import walkingkooka.spreadsheet.dominokit.RefreshContextDelegator;
 import walkingkooka.spreadsheet.dominokit.fetcher.DecimalNumberSymbolsFetcherWatcher;
 import walkingkooka.spreadsheet.dominokit.fetcher.SpreadsheetDeltaFetcherWatcher;
 import walkingkooka.spreadsheet.dominokit.fetcher.SpreadsheetMetadataFetcherWatcher;
-import walkingkooka.spreadsheet.dominokit.history.HistoryContext;
-import walkingkooka.spreadsheet.dominokit.history.HistoryContextDelegator;
-import walkingkooka.spreadsheet.dominokit.log.LoggingContext;
-import walkingkooka.spreadsheet.dominokit.log.LoggingContextDelegator;
 
 abstract class AppContextDecimalNumberSymbolsDialogComponentContext implements DecimalNumberSymbolsDialogComponentContext,
-    HistoryContextDelegator,
-    LoggingContextDelegator {
+    RefreshContextDelegator {
 
     AppContextDecimalNumberSymbolsDialogComponentContext(final AppContext context) {
         this.context = context;
@@ -55,24 +52,10 @@ abstract class AppContextDecimalNumberSymbolsDialogComponentContext implements D
         return this.context.addSpreadsheetMetadataFetcherWatcher(watcher);
     }
 
-    // CanGiveFocus.....................................................................................................
+    // RefreshContextDelegator..........................................................................................
 
     @Override
-    public final void giveFocus(final Runnable focus) {
-        this.context.giveFocus(focus);
-    }
-
-    // HistoryContextDelegator..........................................................................................
-
-    @Override
-    public final HistoryContext historyContext() {
-        return this.context;
-    }
-
-    // LoggingContextDelegator..........................................................................................
-
-    @Override
-    public final LoggingContext loggingContext() {
+    public final RefreshContext refreshContext() {
         return this.context;
     }
 
