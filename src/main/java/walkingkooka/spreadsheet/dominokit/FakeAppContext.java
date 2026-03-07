a0cdde69df1ff5e6a262d81a911da590c38f5b62
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-07 18:56:18 +1100

    https://github.com/mP1/walkingkooka-environment/pull/330 EnvironmentContext.addEnvironmentValueWatcherXXX was addEventValueWatcherXXX
    - https://github.com/mP1/walkingkooka-environment/pull/330
    - EnvironmentContext.addEnvironmentValueWatcherXXX was addEventValueWatcherXXX

--- src/main/java/walkingkooka/spreadsheet/dominokit/FakeAppContext.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/FakeAppContext.java
@@ -492,12 +492,12 @@ public class FakeAppContext extends FakeSpreadsheetProvider
     }
 
     @Override
-    public Runnable addEventValueWatcher(final EnvironmentValueWatcher watcher) {
+    public Runnable addEnvironmentValueWatcher(final EnvironmentValueWatcher watcher) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Runnable addEventValueWatcherOnce(final EnvironmentValueWatcher watcher) {
+    public Runnable addEnvironmentValueWatcherOnce(final EnvironmentValueWatcher watcher) {
         throw new UnsupportedOperationException();
     }
 
--- src/main/java/walkingkooka/spreadsheet/dominokit/find/FakeSpreadsheetCellFindDialogComponentContext.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/find/FakeSpreadsheetCellFindDialogComponentContext.java
@@ -170,12 +170,12 @@ public class FakeSpreadsheetCellFindDialogComponentContext extends FakeDialogCom
     }
 
     @Override
-    public Runnable addEventValueWatcher(final EnvironmentValueWatcher watcher) {
+    public Runnable addEnvironmentValueWatcher(final EnvironmentValueWatcher watcher) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Runnable addEventValueWatcherOnce(final EnvironmentValueWatcher watcher) {
+    public Runnable addEnvironmentValueWatcherOnce(final EnvironmentValueWatcher watcher) {
         throw new UnsupportedOperationException();
     }
 
