91f9652dcf7f4c5158d2c1085b82b80a1607dcc9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-01 17:19:26 +1100

    KeyBindingsTableComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/key/KeyBindingTableComponent.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.key;
+
+import elemental2.dom.HTMLDivElement;
+import org.dominokit.domino.ui.datatable.CellTextAlign;
+import org.dominokit.domino.ui.datatable.ColumnConfig;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.spreadsheet.dominokit.HtmlComponent;
+import walkingkooka.spreadsheet.dominokit.HtmlComponentDelegator;
+import walkingkooka.spreadsheet.dominokit.card.CardComponent;
+import walkingkooka.spreadsheet.dominokit.datatable.DataTableComponent;
+import walkingkooka.spreadsheet.dominokit.value.TableComponent;
+import walkingkooka.text.CharSequences;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.tree.text.TextAlign;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * A table that displays {@link KeyBinding} with one per row.
+ */
+public final class KeyBindingTableComponent implements TableComponent<HTMLDivElement, List<KeyBinding>, KeyBindingTableComponent>,
+    HtmlComponentDelegator<HTMLDivElement, KeyBindingTableComponent> {
+
+    /**
+     * Creates an empty {@link KeyBindingTableComponent}.
+     */
+    public static KeyBindingTableComponent empty(final String id) {
+        return new KeyBindingTableComponent(
+            CharSequences.failIfNullOrEmpty(id, "id")
+        );
+    }
+
+    private KeyBindingTableComponent(final String id) {
+        this.cellRenderer = KeyBindingTableComponentDataTableComponentCellRenderer.INSTANCE;
+        this.dataTable = DataTableComponent.with(
+            id, // id
+            columnConfigs(), // configs
+            this.cellRenderer
+        ).hideHeaders();
+
+        this.card = CardComponent.empty()
+            .appendChild(this.dataTable);
+    }
+
+    private static List<ColumnConfig<KeyBinding>> columnConfigs() {
+        final List<ColumnConfig<KeyBinding>> columns = Lists.array();
+
+        columns.add(
+            columnConfig(
+                "Command/Action",
+                TextAlign.LEFT
+            )
+        );
+
+        columns.add(
+            columnConfig(
+                "Key",
+                TextAlign.CENTER
+            )
+        );
+
+        return columns;
+    }
+
+    private static ColumnConfig<KeyBinding> columnConfig(final String columnName,
+                                                         final TextAlign textAlign) {
+        return ColumnConfig.<KeyBinding>create(columnName)
+            .setFixed(true)
+            .minWidth("50%")
+            .setTextAlign(
+                CellTextAlign.valueOf(
+                    textAlign.name()
+                )
+            );
+    }
+
+    @Override
+    public KeyBindingTableComponent focus() {
+        this.dataTable.focus();
+        return this;
+    }
+
+    @Override
+    public KeyBindingTableComponent blur() {
+        this.dataTable.blur();
+        return this;
+    }
+
+    @Override
+    public Optional<List<KeyBinding>> value() {
+        return this.bindings;
+    }
+
+    @Override
+    public KeyBindingTableComponent setValue(final Optional<List<KeyBinding>> bindings) {
+        this.bindings = Objects.requireNonNull(bindings, "bindings")
+            .map(KeyBindingTableComponent::sort);
+        return this;
+    }
+
+    private static List<KeyBinding> sort(final List<KeyBinding> bindings) {
+        final List<KeyBinding> sorted = Lists.array();
+        sorted.addAll(bindings);
+        sorted.sort(KeyBinding::compareTo);
+        return sorted;
+    }
+
+    private Optional<List<KeyBinding>> bindings;
+
+    public void refresh(final KeyBindingTableComponentContext context) {
+        this.dataTable.setValue(this.bindings);
+    }
+
+    private final DataTableComponent<KeyBinding> dataTable;
+
+    private final KeyBindingTableComponentDataTableComponentCellRenderer cellRenderer;
+
+    // HtmlComponentDelegator...........................................................................................
+
+    @Override
+    public HtmlComponent<HTMLDivElement, ?> htmlComponent() {
+        return this.card;
+    }
+
+    private final CardComponent card;
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        printer.println(this.getClass().getSimpleName());
+        printer.indent();
+        {
+            this.card.printTree(printer);
+        }
+        printer.outdent();
+    }
+}
---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/key/KeyBindingTableComponentDataTableComponentCellRenderer.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.key;
+
+import walkingkooka.spreadsheet.dominokit.Component;
+import walkingkooka.spreadsheet.dominokit.datatable.DataTableComponentCellRenderer;
+import walkingkooka.spreadsheet.dominokit.text.TextNodeComponent;
+import walkingkooka.tree.text.TextNode;
+
+import java.util.Optional;
+
+final class KeyBindingTableComponentDataTableComponentCellRenderer implements DataTableComponentCellRenderer<KeyBinding> {
+
+    /**
+     * Singleton
+     */
+    final static KeyBindingTableComponentDataTableComponentCellRenderer INSTANCE = new KeyBindingTableComponentDataTableComponentCellRenderer();
+
+    private KeyBindingTableComponentDataTableComponentCellRenderer() {
+        super();
+    }
+
+    @Override
+    public Component render(final int column,
+                            final KeyBinding binding) {
+        final Component rendered;
+
+        switch (column) {
+            case 0:
+                rendered = this.text(
+                    binding.label()
+                );
+                break;
+            case 1:
+                rendered = this.text(
+                    binding.key() +
+                        " " +
+                        binding.toStringModifiers()
+                );
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid column " + column);
+        }
+
+        return rendered;
+    }
+
+    private Component text(final String label) {
+        return TextNodeComponent.with(
+            Optional.of(
+                TextNode.text(label)
+            )
+        );
+    }
+}
