a9daf171fd64b7109afe4179f1edcfafe84616b9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-12 12:17:20 +1000

    TextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegatorTest.testPrintTree()

--- src/test/java/walkingkooka/spreadsheet/dominokit/textstyle/TextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegatorTest.java
+++ src/test/java/walkingkooka/spreadsheet/dominokit/textstyle/TextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegatorTest.java
@@ -18,18 +18,39 @@
 package walkingkooka.spreadsheet.dominokit.textstyle;
 
 import elemental2.dom.HTMLDivElement;
+import org.junit.jupiter.api.Test;
 import walkingkooka.Cast;
 import walkingkooka.collect.list.Lists;
 import walkingkooka.naming.HasName;
 import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.spreadsheet.dominokit.HtmlComponent;
 import walkingkooka.spreadsheet.dominokit.HtmlComponentTesting;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenWatcher;
 import walkingkooka.tree.text.TextAlign;
 import walkingkooka.tree.text.TextStylePropertyName;
 
 import java.util.Optional;
 
 public final class TextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegatorTest implements HtmlComponentTesting<TextStylePropertyNameEnumHistoryTokenAnchorListComponent<TextAlign>, HTMLDivElement> {
+
+    @Test
+    public void testPrintTree() {
+        this.treePrintAndCheck(
+            new TestTextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegator(),
+            "TestTextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegator\n" +
+                "  TextStylePropertyNameEnumHistoryTokenAnchorListComponent\n" +
+                "    AnchorListComponent\n" +
+                "      FlexLayoutComponent\n" +
+                "        ROW\n" +
+                "          \"LEFT\" [#/1/SpreadsheetName111/spreadsheet/style/text-align/save/LEFT] id=Test-textAlign-LEFT-Link\n" +
+                "          \"RIGHT\" [#/1/SpreadsheetName111/spreadsheet/style/text-align/save/RIGHT] id=Test-textAlign-RIGHT-Link\n" +
+                "          \"CENTER\" [#/1/SpreadsheetName111/spreadsheet/style/text-align/save/CENTER] id=Test-textAlign-CENTER-Link\n" +
+                "          \"JUSTIFY\" [#/1/SpreadsheetName111/spreadsheet/style/text-align/save/JUSTIFY] id=Test-textAlign-JUSTIFY-Link\n"
+        );
+    }
+
+    // class............................................................................................................
 
     @Override
     public void testTestNaming() {
@@ -54,14 +75,25 @@ public final class TextStylePropertyNameEnumHistoryTokenAnchorListComponentDeleg
         public TextStylePropertyNameEnumHistoryTokenAnchorListComponent<TextAlign> textStylePropertyNameEnumHistoryTokenAnchorListComponent() {
             return this.component;
         }
-        
+
         private final TextStylePropertyNameEnumHistoryTokenAnchorListComponent<TextAlign> component = TextStylePropertyNameEnumHistoryTokenAnchorListComponent.with(
             "Test-",
             TextStylePropertyName.TEXT_ALIGN,
             Lists.of(TextAlign.values()),
             (n) -> n.map(TextAlign::name).orElse("Clear"),
             (n) -> Optional.empty(),
-            new FakeTextStylePropertyNameEnumHistoryTokenAnchorListComponentContext()
+            new FakeTextStylePropertyNameEnumHistoryTokenAnchorListComponentContext() {
+                @Override
+                public Runnable addHistoryTokenWatcher(final HistoryTokenWatcher watcher) {
+                    return () -> {
+                    };
+                }
+
+                @Override
+                public HistoryToken historyToken() {
+                    return HistoryToken.parseString("/1/SpreadsheetName111");
+                }
+            }
         );
     }
 }
