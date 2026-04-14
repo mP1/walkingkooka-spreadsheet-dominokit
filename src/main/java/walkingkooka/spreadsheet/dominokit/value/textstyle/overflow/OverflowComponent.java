be13cafc488c2efbccace8127ce348d4f55bf31f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-14 17:55:19 +1000

    OverflowComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/7532
    - OverflowXComponent
    - https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/7533
    - OverflowYComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/overflow/OverflowComponent.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.overflow;
+
+import walkingkooka.collect.list.Lists;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.TextStylePropertyNameEnumHistoryTokenAnchorListComponent;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.TextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegator;
+import walkingkooka.tree.text.Overflow;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+public final class OverflowComponent implements TextStylePropertyNameEnumHistoryTokenAnchorListComponentDelegator<Overflow, OverflowComponent> {
+
+    public static OverflowComponent overflowX(final String idPrefix,
+                                              final OverflowComponentContext context) {
+        return new OverflowComponent(
+            idPrefix,
+            TextStylePropertyName.OVERFLOW_X,
+            context
+        );
+    }
+
+    public static OverflowComponent overflowY(final String idPrefix,
+                                              final OverflowComponentContext context) {
+        return new OverflowComponent(
+            idPrefix,
+            TextStylePropertyName.OVERFLOW_Y,
+            context
+        );
+    }
+
+    private static OverflowComponent with(final String idPrefix,
+                                          final TextStylePropertyName<Overflow> propertyName,
+                                          final OverflowComponentContext context) {
+        return new OverflowComponent(
+            idPrefix,
+            propertyName,
+            context
+        );
+    }
+
+    private OverflowComponent(final String idPrefix,
+                              final TextStylePropertyName<Overflow> propertyName,
+                              final OverflowComponentContext context) {
+        super();
+        this.component = TextStylePropertyNameEnumHistoryTokenAnchorListComponent.with(
+            idPrefix,
+            propertyName,
+            Lists.of(
+                null,
+                Overflow.VISIBLE,
+                Overflow.HIDDEN,
+                Overflow.SCROLL,
+                Overflow.AUTO
+            ),
+            TextStylePropertyNameEnumHistoryTokenAnchorListComponent.valueToText(),
+            TextStylePropertyNameEnumHistoryTokenAnchorListComponent.noIcons(),
+            context // TextStylePropertyNameEnumHistoryTokenAnchorListComponentContext
+        );
+    }
+
+    @Override
+    public TextStylePropertyNameEnumHistoryTokenAnchorListComponent<Overflow> textStylePropertyNameEnumHistoryTokenAnchorListComponent() {
+        return this.component;
+    }
+
+    private final TextStylePropertyNameEnumHistoryTokenAnchorListComponent<Overflow> component;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.component.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/overflow/OverflowComponentTest.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.overflow;
+
+import elemental2.dom.HTMLFieldSetElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.color.Color;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.HtmlComponentTesting;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenWatcher;
+import walkingkooka.spreadsheet.meta.SpreadsheetId;
+import walkingkooka.spreadsheet.meta.SpreadsheetName;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+import walkingkooka.tree.text.Overflow;
+import walkingkooka.tree.text.TextStyle;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+import java.util.Optional;
+
+public final class OverflowComponentTest implements HtmlComponentTesting<OverflowComponent, HTMLFieldSetElement> {
+
+    @Test
+    public void testSetValue() {
+        final OverflowComponent component = OverflowComponent.overflowX(
+            "Test123-",
+            new FakeOverflowComponentContext() {
+                @Override
+                public Runnable addHistoryTokenWatcher(final HistoryTokenWatcher watcher) {
+                    return () -> {};
+                }
+
+                @Override
+                public HistoryToken historyToken() {
+                    return HistoryToken.cellStyle(
+                        SpreadsheetId.with(1),
+                        SpreadsheetName.with("SpreadsheetName111"),
+                        SpreadsheetSelection.A1.setDefaultAnchor(),
+                        Optional.of(
+                            TextStylePropertyName.OUTLINE_STYLE
+                        )
+                    );
+                }
+            }
+        );
+
+        component.setValue(
+            Optional.of(
+                Overflow.HIDDEN
+            )
+        );
+
+        this.treePrintAndCheck(
+            component,
+            "OverflowComponent\n" +
+                "  TextStylePropertyNameEnumHistoryTokenAnchorListComponent\n" +
+                "    Overflow X\n" +
+                "      AnchorListComponent\n" +
+                "        FlexLayoutComponent\n" +
+                "          ROW\n" +
+                "            \"Clear\" [#/1/SpreadsheetName111/cell/A1/style/overflow-x/save/] id=Test123-overflowX-Link\n" +
+                "            \"Visible\" [#/1/SpreadsheetName111/cell/A1/style/overflow-x/save/VISIBLE] id=Test123-overflowX-VISIBLE-Link\n" +
+                "            \"Hidden\" [#/1/SpreadsheetName111/cell/A1/style/overflow-x/save/HIDDEN] CHECKED id=Test123-overflowX-HIDDEN-Link\n" +
+                "            \"Scroll\" [#/1/SpreadsheetName111/cell/A1/style/overflow-x/save/SCROLL] id=Test123-overflowX-SCROLL-Link\n" +
+                "            \"Auto\" [#/1/SpreadsheetName111/cell/A1/style/overflow-x/save/AUTO] id=Test123-overflowX-AUTO-Link\n"
+        );
+    }
+
+    @Test
+    public void testTextStyleValueWatcherOnValueChange() {
+        final OverflowComponent component = OverflowComponent.overflowY(
+            "Test123-",
+            new FakeOverflowComponentContext() {
+                @Override
+                public Runnable addHistoryTokenWatcher(final HistoryTokenWatcher watcher) {
+                    return () -> {};
+                }
+
+                @Override
+                public HistoryToken historyToken() {
+                    return HistoryToken.cellStyle(
+                        SpreadsheetId.with(1),
+                        SpreadsheetName.with("SpreadsheetName111"),
+                        SpreadsheetSelection.A1.setDefaultAnchor(),
+                        Optional.of(
+                            TextStylePropertyName.OUTLINE_STYLE
+                        )
+                    );
+                }
+            }
+        );
+
+        component.textStyleValueWatcher()
+            .onValue(
+                Optional.of(
+                    TextStyle.EMPTY.set(
+                        TextStylePropertyName.OVERFLOW_X,
+                        Overflow.VISIBLE
+                    ).set(
+                        TextStylePropertyName.OVERFLOW_Y,
+                        Overflow.HIDDEN
+                    ).set(
+                        TextStylePropertyName.COLOR,
+                        Color.WHITE
+                    )
+                )
+            );
+
+        this.treePrintAndCheck(
+            component,
+            "OverflowComponent\n" +
+                "  TextStylePropertyNameEnumHistoryTokenAnchorListComponent\n" +
+                "    Overflow Y\n" +
+                "      AnchorListComponent\n" +
+                "        FlexLayoutComponent\n" +
+                "          ROW\n" +
+                "            \"Clear\" [#/1/SpreadsheetName111/cell/A1/style/overflow-y/save/] id=Test123-overflowY-Link\n" +
+                "            \"Visible\" [#/1/SpreadsheetName111/cell/A1/style/overflow-y/save/VISIBLE] id=Test123-overflowY-VISIBLE-Link\n" +
+                "            \"Hidden\" [#/1/SpreadsheetName111/cell/A1/style/overflow-y/save/HIDDEN] CHECKED id=Test123-overflowY-HIDDEN-Link\n" +
+                "            \"Scroll\" [#/1/SpreadsheetName111/cell/A1/style/overflow-y/save/SCROLL] id=Test123-overflowY-SCROLL-Link\n" +
+                "            \"Auto\" [#/1/SpreadsheetName111/cell/A1/style/overflow-y/save/AUTO] id=Test123-overflowY-AUTO-Link\n"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<OverflowComponent> type() {
+        return OverflowComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
