62bd9f10712269f41a8fb0ef82eac413c46d634e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-14 19:42:17 +1000

    TextWrappingComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/7520
    - TextWrappingComponent

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/textwrapping/TextWrappingComponentTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.textwrapping;
+
+import elemental2.dom.HTMLFieldSetElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.color.Color;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.HtmlComponentTesting;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.history.HistoryTokenWatcher;
+import walkingkooka.spreadsheet.meta.SpreadsheetId;
+import walkingkooka.spreadsheet.meta.SpreadsheetName;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+import walkingkooka.tree.text.TextStyle;
+import walkingkooka.tree.text.TextStylePropertyName;
+import walkingkooka.tree.text.TextWrapping;
+
+import java.util.Optional;
+
+public final class TextWrappingComponentTest implements HtmlComponentTesting<TextWrappingComponent, HTMLFieldSetElement> {
+
+    @Test
+    public void testSetValue() {
+        final TextWrappingComponent component = TextWrappingComponent.with(
+            "Test123-",
+            new FakeTextWrappingComponentContext() {
+                @Override
+                public Runnable addHistoryTokenWatcher(final HistoryTokenWatcher watcher) {
+                    return () -> {};
+                }
+
+                @Override
+                public HistoryToken historyToken() {
+                    return HistoryToken.cellStyle(
+                        SpreadsheetId.with(1),
+                        SpreadsheetName.with("SpreadsheetName111"),
+                        SpreadsheetSelection.A1.setDefaultAnchor(),
+                        Optional.of(
+                            TextStylePropertyName.TEXT_WRAPPING
+                        )
+                    );
+                }
+            }
+        );
+
+        component.setValue(
+            Optional.of(
+                TextWrapping.WRAP
+            )
+        );
+
+        this.treePrintAndCheck(
+            component,
+            "TextWrappingComponent\n" +
+                "  TextStylePropertyNameEnumHistoryTokenAnchorListComponent\n" +
+                "    Text Wrapping\n" +
+                "      AnchorListComponent\n" +
+                "        FlexLayoutComponent\n" +
+                "          ROW\n" +
+                "            \"Clear\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/] id=Test123-textWrapping-Link\n" +
+                "            \"Overflow\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/OVERFLOW] id=Test123-textWrapping-OVERFLOW-Link\n" +
+                "            \"Wrap\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/WRAP] CHECKED id=Test123-textWrapping-WRAP-Link\n" +
+                "            \"Clip\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/CLIP] id=Test123-textWrapping-CLIP-Link\n"
+        );
+    }
+
+    @Test
+    public void testTextStyleValueWatcherOnValueChange() {
+        final TextWrappingComponent component = TextWrappingComponent.with(
+            "Test123-",
+            new FakeTextWrappingComponentContext() {
+                @Override
+                public Runnable addHistoryTokenWatcher(final HistoryTokenWatcher watcher) {
+                    return () -> {};
+                }
+
+                @Override
+                public HistoryToken historyToken() {
+                    return HistoryToken.cellStyle(
+                        SpreadsheetId.with(1),
+                        SpreadsheetName.with("SpreadsheetName111"),
+                        SpreadsheetSelection.A1.setDefaultAnchor(),
+                        Optional.of(
+                            TextStylePropertyName.TEXT_WRAPPING
+                        )
+                    );
+                }
+            }
+        );
+
+        component.textStyleValueWatcher()
+            .onValue(
+                Optional.of(
+                    TextStyle.EMPTY.set(
+                        TextStylePropertyName.TEXT_WRAPPING,
+                        TextWrapping.OVERFLOW
+                    ).set(
+                        TextStylePropertyName.COLOR,
+                        Color.WHITE
+                    )
+                )
+            );
+
+        this.treePrintAndCheck(
+            component,
+            "TextWrappingComponent\n" +
+                "  TextStylePropertyNameEnumHistoryTokenAnchorListComponent\n" +
+                "    Text Wrapping\n" +
+                "      AnchorListComponent\n" +
+                "        FlexLayoutComponent\n" +
+                "          ROW\n" +
+                "            \"Clear\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/] id=Test123-textWrapping-Link\n" +
+                "            \"Overflow\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/OVERFLOW] CHECKED id=Test123-textWrapping-OVERFLOW-Link\n" +
+                "            \"Wrap\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/WRAP] id=Test123-textWrapping-WRAP-Link\n" +
+                "            \"Clip\" [#/1/SpreadsheetName111/cell/A1/style/text-wrapping/save/CLIP] id=Test123-textWrapping-CLIP-Link\n"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<TextWrappingComponent> type() {
+        return TextWrappingComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
