4e8b8549784a8fe5cbd7d18e5c85e90be1f8d480
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-17 07:21:05 +1000

    OpacityComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/7528
    - OpacityComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/opacity/OpacityComponent.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.opacity;
+
+import walkingkooka.spreadsheet.dominokit.value.ValueTextBoxComponent;
+import walkingkooka.spreadsheet.dominokit.value.ValueTextBoxComponentDelegator;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.tree.text.Opacity;
+
+/**
+ * A text box that accepts text entry and validates it as a {@link Opacity}.
+ */
+public final class OpacityComponent implements ValueTextBoxComponentDelegator<OpacityComponent, Opacity> {
+
+    public static OpacityComponent empty() {
+        return new OpacityComponent();
+    }
+
+    private OpacityComponent() {
+        this.textBox = ValueTextBoxComponent.with(
+            Opacity::parse,
+            Opacity::text
+        );
+    }
+
+    // ValueTextBoxComponentDelegator...................................................................................
+
+    @Override
+    public ValueTextBoxComponent<Opacity> valueTextBoxComponent() {
+        return this.textBox;
+    }
+
+    private final ValueTextBoxComponent<Opacity> textBox;
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        printer.println(this.getClass().getSimpleName());
+        printer.indent();
+        {
+            this.valueTextBoxComponent()
+                .printTree(printer);
+        }
+        printer.outdent();
+    }
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.textBox.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/opacity/OpacityComponentTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.opacity;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.value.ValueTextBoxComponentLikeTesting;
+import walkingkooka.tree.text.Opacity;
+
+import java.util.Optional;
+
+public final class OpacityComponentTest implements ValueTextBoxComponentLikeTesting<OpacityComponent, Opacity> {
+    
+    private final static Opacity OPACITY = Opacity.parse("50%");
+
+    @Test
+    public void testClearValue() {
+        this.treePrintAndCheck(
+            OpacityComponent.empty()
+                .clearValue(),
+            "OpacityComponent\n" +
+                "  ValueTextBoxComponent\n" +
+                "    TextBoxComponent\n" +
+                "      [] REQUIRED\n" +
+                "      Errors\n" +
+                "        Empty \"text\"\n"
+        );
+    }
+
+    @Test
+    public void testClearValueOpacity() {
+        this.treePrintAndCheck(
+            OpacityComponent.empty()
+                .optional()
+                .clearValue(),
+            "OpacityComponent\n" +
+                "  ValueTextBoxComponent\n" +
+                "    TextBoxComponent\n" +
+                "      []\n"
+        );
+    }
+
+    @Test
+    public void testSetValue() {
+        this.treePrintAndCheck(
+            OpacityComponent.empty()
+                .setValue(
+                    Optional.of(OPACITY)
+                ),
+            "OpacityComponent\n" +
+                "  ValueTextBoxComponent\n" +
+                "    TextBoxComponent\n" +
+                "      [0.5] REQUIRED\n"
+        );
+    }
+
+    @Test
+    public void testSetStringValueWithInvalid() {
+        this.treePrintAndCheck(
+            OpacityComponent.empty()
+                .setStringValue(
+                    Optional.of(
+                        "Invalid123!"
+                    )
+                ),
+            "OpacityComponent\n" +
+                "  ValueTextBoxComponent\n" +
+                "    TextBoxComponent\n" +
+                "      [Invalid123!] REQUIRED\n" +
+                "      Errors\n" +
+                "        Invalid character 'I' at 0\n"
+        );
+    }
+
+    // ValueComponent...................................................................................................
+
+    @Override
+    public OpacityComponent createComponent() {
+        return OpacityComponent.empty();
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<OpacityComponent> type() {
+        return OpacityComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
