26bc643df1f75d4aa2dc1e6f20461653187a97a3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-17 13:39:34 +1000

    TextStyleLengthPropertyComponentLikeTesting

---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/length/TextStyleLengthPropertyComponentLikeTesting.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.length;
+
+import elemental2.dom.HTMLFieldSetElement;
+import walkingkooka.naming.HasNameTesting;
+import walkingkooka.spreadsheet.dominokit.value.ValueComponentTesting;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.TextStyleLengthPropertyComponentLike;
+import walkingkooka.tree.text.Length;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+public interface TextStyleLengthPropertyComponentLikeTesting<C extends TextStyleLengthPropertyComponentLike<C>> extends ValueComponentTesting<HTMLFieldSetElement, Length<?>, C>,
+    HasNameTesting<TextStylePropertyName<Length<?>>> {
+}
