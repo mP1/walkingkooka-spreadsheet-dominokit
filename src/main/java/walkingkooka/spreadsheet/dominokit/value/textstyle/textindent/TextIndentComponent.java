c25703262cd77639beefacd0fa7808d7b2e084ae
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-17 12:57:54 +1000

    TextIndentComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/7523
    - TextIndentComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/textindent/TextIndentComponent.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.textindent;
+
+import elemental2.dom.HTMLFieldSetElement;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.HasTextStyleValueWatcherValueComponent;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.length.LengthComponent;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.length.LengthComponentDelegator;
+import walkingkooka.tree.text.Length;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+public final class TextIndentComponent implements LengthComponentDelegator<TextIndentComponent>,
+    HasTextStyleValueWatcherValueComponent<HTMLFieldSetElement, Length<?>, TextIndentComponent> {
+
+    public static TextIndentComponent empty() {
+        return new TextIndentComponent();
+    }
+
+    private TextIndentComponent() {
+        this.lengthComponent = LengthComponent.empty();
+    }
+
+    @Override
+    public TextStylePropertyName<Length<?>> name() {
+        return TextStylePropertyName.TEXT_INDENT;
+    }
+
+    // LengthComponentDelegator.........................................................................................
+
+    @Override
+    public LengthComponent lengthComponent() {
+        return this.lengthComponent;
+    }
+
+    private final LengthComponent lengthComponent;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.lengthComponent.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/textindent/TextIndentComponentTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.textindent;
+
+import elemental2.dom.HTMLFieldSetElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.value.FormValueComponentTesting;
+import walkingkooka.tree.text.Length;
+
+import java.util.Optional;
+
+public final class TextIndentComponentTest implements FormValueComponentTesting<HTMLFieldSetElement, Length<?>, TextIndentComponent> {
+
+    private final static Length<?> LENGTH = Length.parse("1px");
+
+    @Test
+    public void testClearValue() {
+        this.treePrintAndCheck(
+            TextIndentComponent.empty()
+                .clearValue(),
+            "TextIndentComponent\n" +
+                "  LengthComponent\n" +
+                "    ValueTextBoxComponent\n" +
+                "      TextBoxComponent\n" +
+                "        [] REQUIRED\n" +
+                "        Errors\n" +
+                "          Empty \"text\"\n"
+        );
+    }
+
+    @Test
+    public void testSetValue() {
+        this.treePrintAndCheck(
+            TextIndentComponent.empty()
+                .setValue(
+                    Optional.of(LENGTH)
+                ),
+            "TextIndentComponent\n" +
+                "  LengthComponent\n" +
+                "    ValueTextBoxComponent\n" +
+                "      TextBoxComponent\n" +
+                "        [1px] REQUIRED\n"
+        );
+    }
+
+    // ValueComponent...................................................................................................
+
+    @Override
+    public TextIndentComponent createComponent() {
+        return TextIndentComponent.empty();
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<TextIndentComponent> type() {
+        return TextIndentComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
