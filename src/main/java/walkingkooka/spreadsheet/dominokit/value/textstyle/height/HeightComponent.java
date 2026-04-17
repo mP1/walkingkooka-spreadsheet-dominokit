f1db8930e49b2e72cd1646fa44d4ebf3dc2f0a87
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-17 15:34:40 +1000

    HeightComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/7519
    - HeightComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/height/HeightComponent.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.height;
+
+import walkingkooka.spreadsheet.dominokit.value.textstyle.TextStyleLengthPropertyComponentLike;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.length.LengthComponent;
+import walkingkooka.tree.text.Length;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+public final class HeightComponent implements TextStyleLengthPropertyComponentLike<HeightComponent> {
+
+    public static HeightComponent empty(final String idPrefix) {
+        return new HeightComponent(idPrefix);
+    }
+
+    private HeightComponent(final String idPrefix) {
+        this.lengthComponent = LengthComponent.empty();
+
+        this.setIdPrefix(idPrefix);
+    }
+
+    @Override
+    public TextStylePropertyName<Length<?>> name() {
+        return TextStylePropertyName.HEIGHT;
+    }
+
+    // LengthComponentDelegator.........................................................................................
+
+    @Override
+    public LengthComponent lengthComponent() {
+        return this.lengthComponent;
+    }
+
+    private final LengthComponent lengthComponent;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.lengthComponent.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/height/HeightComponentTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.height;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.length.TextStyleLengthPropertyComponentLikeTesting;
+import walkingkooka.tree.text.Length;
+
+import java.util.Optional;
+
+public final class HeightComponentTest implements TextStyleLengthPropertyComponentLikeTesting<HeightComponent> {
+
+    private final static String ID_PREFIX = "TestIdPrefix123-";
+
+    private final static Length<?> LENGTH = Length.parse("1px");
+
+    @Test
+    public void testClearValue() {
+        this.treePrintAndCheck(
+            this.createComponent()
+                .clearValue(),
+            "HeightComponent\n" +
+                "  LengthComponent\n" +
+                "    ValueTextBoxComponent\n" +
+                "      TextBoxComponent\n" +
+                "        [] id=TestIdPrefix123-height-TextBox REQUIRED\n" +
+                "        Errors\n" +
+                "          Empty \"text\"\n"
+        );
+    }
+
+    @Test
+    public void testSetValue() {
+        this.treePrintAndCheck(
+            this.createComponent()
+                .setValue(
+                    Optional.of(LENGTH)
+                ),
+            "HeightComponent\n" +
+                "  LengthComponent\n" +
+                "    ValueTextBoxComponent\n" +
+                "      TextBoxComponent\n" +
+                "        [1px] id=TestIdPrefix123-height-TextBox REQUIRED\n"
+        );
+    }
+
+    // ValueComponent...................................................................................................
+
+    @Override
+    public HeightComponent createComponent() {
+        return HeightComponent.empty(ID_PREFIX);
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<HeightComponent> type() {
+        return HeightComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
