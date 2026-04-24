9af0e80be100b01fcebc16088e32766b219cd743
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-24 14:30:21 +1000

    FontFamilyComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/7504
    - FontFamilyComponent

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/fontfamily/FontFamilyComponent.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.fontfamily;
+
+import elemental2.dom.HTMLFieldSetElement;
+import walkingkooka.spreadsheet.dominokit.SpreadsheetElementIds;
+import walkingkooka.spreadsheet.dominokit.select.SelectComponent;
+import walkingkooka.spreadsheet.dominokit.select.SelectComponentDelegator;
+import walkingkooka.spreadsheet.dominokit.value.FormValueComponent;
+import walkingkooka.text.CaseKind;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.TreePrintable;
+import walkingkooka.tree.text.FontFamily;
+
+import java.util.Optional;
+
+/**
+ * A drop down that supports picking an optional {@link FontFamily}.
+ */
+public final class FontFamilyComponent implements FormValueComponent<HTMLFieldSetElement, FontFamily, FontFamilyComponent>,
+    SelectComponentDelegator<FontFamily, FontFamilyComponent>,
+    TreePrintable {
+
+    public static FontFamilyComponent empty(final String idPrefix,
+                                            final FontFamilyComponentContext context) {
+        return new FontFamilyComponent(
+            idPrefix + "FontFamily",
+            context
+        );
+    }
+
+    private FontFamilyComponent(final String idPrefix,
+                                final FontFamilyComponentContext context) {
+        final SelectComponent<FontFamily> select = SelectComponent.empty(
+            (v) -> {
+                final FontFamily fontFamily = v.orElseThrow(() -> new IllegalArgumentException("Missing FontFamily"));
+                final String nameText = fontFamily.text();
+
+                return context.selectOption(
+                    idPrefix + '-' + CaseKind.TITLE.change(
+                        nameText,
+                        CaseKind.CAMEL
+                    ) + SpreadsheetElementIds.OPTION, // id
+                    nameText, // text
+                    v, // value
+                    Optional.empty() // HistoryToken
+                );
+            }
+        );
+
+        for (final FontFamily fontFamily : context.fontFamilies()) {
+            select.appendOption(
+                Optional.of(fontFamily)
+            );
+        }
+
+        this.select = select;
+        this.setId(idPrefix + SpreadsheetElementIds.SELECT);
+    }
+
+    // SelectComponentDelegator.........................................................................................
+
+    @Override
+    public SelectComponent<FontFamily> selectComponent() {
+        return this.select;
+    }
+
+    private final SelectComponent<FontFamily> select;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.select.toString();
+    }
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        printer.println(this.getClass().getSimpleName());
+        printer.indent();
+        {
+            this.select.printTree(printer);
+        }
+        printer.outdent();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/fontfamily/FontFamilyComponentTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle.fontfamily;
+
+import elemental2.dom.HTMLFieldSetElement;
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.history.HistoryToken;
+import walkingkooka.spreadsheet.dominokit.value.FormValueComponentTesting;
+import walkingkooka.spreadsheet.meta.SpreadsheetId;
+import walkingkooka.spreadsheet.meta.SpreadsheetName;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+import walkingkooka.tree.text.FontFamily;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+import java.util.List;
+import java.util.Optional;
+
+public final class FontFamilyComponentTest implements FormValueComponentTesting<HTMLFieldSetElement, FontFamily, FontFamilyComponent> {
+
+    private final static FontFamily COURIER = FontFamily.with("Courier");
+
+    @Test
+    public void testClearValue() {
+        this.treePrintAndCheck(
+            this.createComponent(),
+            "FontFamilyComponent\n" +
+                "  SelectComponent\n" +
+                "    [] id=FontFamily123-FontFamily-Select\n" +
+                "      \"Courier\" DISABLED id=FontFamily123-FontFamily-courier-Option\n" +
+                "      \"Helvetica\" DISABLED id=FontFamily123-FontFamily-helvetica-Option\n" +
+                "      \"Times New Roman\" DISABLED id=FontFamily123-FontFamily-timesNewRoman-Option\n"
+        );
+    }
+
+    @Test
+    public void testSetValue() {
+        this.treePrintAndCheck(
+            this.createComponent()
+                .setValue(
+                    Optional.of(COURIER)
+                ),
+            "FontFamilyComponent\n" +
+                "  SelectComponent\n" +
+                "    [Courier] id=FontFamily123-FontFamily-Select\n" +
+                "      \"Courier\" DISABLED id=FontFamily123-FontFamily-courier-Option\n" +
+                "      \"Helvetica\" DISABLED id=FontFamily123-FontFamily-helvetica-Option\n" +
+                "      \"Times New Roman\" DISABLED id=FontFamily123-FontFamily-timesNewRoman-Option\n"
+        );
+    }
+
+    // ValueComponent...................................................................................................
+
+    @Override
+    public FontFamilyComponent createComponent() {
+        return FontFamilyComponent.empty(
+            "FontFamily123-",
+            new FakeFontFamilyComponentContext() {
+
+                @Override
+                public HistoryToken historyToken() {
+                    return HistoryToken.cellStyle(
+                        SpreadsheetId.with(1),
+                        SpreadsheetName.with("SpreadsheetName111"),
+                        SpreadsheetSelection.A1.setDefaultAnchor(),
+                        Optional.of(
+                            TextStylePropertyName.FONT_FAMILY
+                        )
+                    );
+                }
+
+                @Override
+                public List<FontFamily> fontFamilies() {
+                    return Lists.of(
+                        COURIER,
+                        FontFamily.with("Helvetica"),
+                        FontFamily.with("Times New Roman")
+                    );
+                }
+            }
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<FontFamilyComponent> type() {
+        return FontFamilyComponent.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
