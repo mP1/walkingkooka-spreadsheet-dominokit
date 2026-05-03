39e702f5e285a1de7104e724ee1638e6e7b22fc3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-05-03 14:51:25 +1000

    TextStyleDialogComponentFilter

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/TextStyleDialogComponentFilter.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle;
+
+import walkingkooka.text.CaseSensitivity;
+import walkingkooka.tree.text.FontFamily;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * A filter that accepts user query and matches {@link TextStylePropertyComponent}. This is used by {@link walkingkooka.spreadsheet.dominokit.value.textstyle.color.TextStyleColorComponent
+ * to support filtering all available {@link TextStylePropertyComponent}.
+ */
+public final class TextStyleDialogComponentFilter {
+
+    public static TextStyleDialogComponentFilter with(final String text) {
+        return new TextStyleDialogComponentFilter(
+            Objects.requireNonNull(text, "text")
+                .trim()
+        );
+    }
+
+    private TextStyleDialogComponentFilter(final String text) {
+        super();
+        this.text = text;
+    }
+
+    public boolean testComponent(final TextStylePropertyComponent<?, ?, ?> component) {
+        return this.testName(component.name()) ||
+            this.testValue(component.value());
+    }
+
+    public boolean testName(final TextStylePropertyName<?> propertyName) {
+        return this.test(
+            propertyName.value()
+        );
+    }
+
+    public boolean testEnums(final Enum<?>[] enumValues) {
+        return Arrays.stream(enumValues)
+            .anyMatch(this::testEnum);
+    }
+
+    public boolean testEnum(final Enum<?> enumValue) {
+        return this.test(enumValue.name());
+    }
+
+    public boolean testFontFamilies(final List<FontFamily> fontFamilies) {
+        return fontFamilies.stream()
+            .anyMatch(this::testFontFamily);
+    }
+
+    public boolean testFontFamily(final FontFamily fontFamily) {
+        return this.test(
+            fontFamily.text()
+        );
+    }
+
+    /**
+     * Tests if the given value matches the filter.
+     */
+    public boolean testValue(final Optional<?> value) {
+        return value.isPresent() &&
+            this.test(
+                value.get()
+                    .toString()
+            );
+    }
+
+    /**
+     * Tests if the given text matches the filter text.
+     */
+    public boolean test(final String value) {
+        final String text = this.text;
+        return text.isEmpty() || CASE_SENSITIVITY.contains(
+            value,
+            text
+        );
+    }
+
+    private final String text;
+
+    private final static CaseSensitivity CASE_SENSITIVITY = CaseSensitivity.INSENSITIVE;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/dominokit/value/textstyle/TextStyleDialogComponentFilterTest.java
@@ -0,0 +1,347 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.textstyle;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.ToStringTesting;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.reflect.ClassTesting;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.dominokit.value.textstyle.opacity.OpacityComponent;
+import walkingkooka.text.CharSequences;
+import walkingkooka.tree.text.FontFamily;
+import walkingkooka.tree.text.TextAlign;
+import walkingkooka.tree.text.TextStylePropertyName;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class TextStyleDialogComponentFilterTest implements ClassTesting<TextStyleDialogComponentFilter>,
+    ToStringTesting<TextStyleDialogComponentFilter> {
+
+    // with.............................................................................................................
+
+    @Test
+    public void testWithNullFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> TextStyleDialogComponentFilter.with(null)
+        );
+    }
+
+    // testTextStylePropertyComponent...................................................................................
+
+    private final static String ID_PREFIX = "TestIdPrefix123-";
+
+    @Test
+    public void testTestComponentWithName() {
+        this.testComponentAndCheck(
+            TextStyleDialogComponentFilter.with("CITY"),
+            OpacityComponent.with(
+                ID_PREFIX
+            ),
+            true
+        );
+    }
+
+    @Test
+    public void testTestComponentWithValue() {
+        this.testComponentAndCheck(
+            TextStyleDialogComponentFilter.with("50"),
+            OpacityComponent.with(
+                ID_PREFIX
+            ).setStringValue(
+                Optional.of("75")
+            ),
+            false
+        );
+    }
+
+    @Test
+    public void testTestComponentFalse() {
+        this.testComponentAndCheck(
+            TextStyleDialogComponentFilter.with("Bad"),
+            OpacityComponent.with(
+                ID_PREFIX
+            ).setStringValue(
+                Optional.of("50")
+            ),
+            false
+        );
+    }
+
+    private void testComponentAndCheck(final TextStyleDialogComponentFilter filter,
+                                       final TextStylePropertyComponent<?, ?, ?> component,
+                                       final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.testComponent(component),
+            () -> filter + " " + component
+        );
+    }
+
+    // testName.........................................................................................................
+
+    @Test
+    public void testTestNameMatch() {
+        this.testNameAndCheck(
+            TextStyleDialogComponentFilter.with("COL"),
+            TextStylePropertyName.BACKGROUND_COLOR,
+            true
+        );
+    }
+
+    @Test
+    public void testTestNameMismatch() {
+        this.testNameAndCheck(
+            TextStyleDialogComponentFilter.with("MIS"),
+            TextStylePropertyName.BACKGROUND_COLOR,
+            false
+        );
+    }
+
+    private void testNameAndCheck(final TextStyleDialogComponentFilter filter,
+                                  final TextStylePropertyName<?> name,
+                                  final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.testName(name),
+            () -> filter + " " + name
+        );
+    }
+
+    // testValue........................................................................................................
+
+    @Test
+    public void testTestValueMatch() {
+        this.testValueAndCheck(
+            TextStyleDialogComponentFilter.with("Lo"),
+            Optional.of("Hello"),
+            true
+        );
+    }
+
+    @Test
+    public void testTestValueMismatch() {
+        this.testValueAndCheck(
+            TextStyleDialogComponentFilter.with("NO"),
+            Optional.of("Hello"),
+            false
+        );
+    }
+
+    @Test
+    public void testTestValueWithNoValue() {
+        this.testValueAndCheck(
+            TextStyleDialogComponentFilter.with("NO"),
+            Optional.empty(),
+            false
+        );
+    }
+
+    private void testValueAndCheck(final TextStyleDialogComponentFilter filter,
+                                   final Optional<?> value,
+                                   final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.testValue(value),
+            () -> filter + " " + value
+        );
+    }
+
+    // testEnums........................................................................................................
+
+    @Test
+    public void testTestEnumsMatch() {
+        this.testEnumsAndCheck(
+            TextStyleDialogComponentFilter.with("LEft"),
+            TextAlign.values(),
+            true
+        );
+    }
+
+    @Test
+    public void testTestEnumsMismatch() {
+        this.testEnumsAndCheck(
+            TextStyleDialogComponentFilter.with("NO"),
+            TextAlign.values(),
+            false
+        );
+    }
+
+    private void testEnumsAndCheck(final TextStyleDialogComponentFilter filter,
+                                   final Enum<?>[] enumValues,
+                                   final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.testEnums(enumValues),
+            () -> filter + " " + Arrays.toString(enumValues)
+        );
+    }
+
+    // testEnum.........................................................................................................
+
+    @Test
+    public void testTestEnumMatch() {
+        this.testEnumAndCheck(
+            TextStyleDialogComponentFilter.with("LEft"),
+            TextAlign.LEFT,
+            true
+        );
+    }
+
+    @Test
+    public void testTestEnumMismatch() {
+        this.testEnumAndCheck(
+            TextStyleDialogComponentFilter.with("NO"),
+            TextAlign.RIGHT,
+            false
+        );
+    }
+
+    private void testEnumAndCheck(final TextStyleDialogComponentFilter filter,
+                                  final Enum<?> enumValue,
+                                  final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.testEnum(enumValue),
+            () -> filter + " " + enumValue
+        );
+    }
+
+    // testFontFamilies.................................................................................................
+
+    @Test
+    public void testTestFontFamiliesMatch() {
+        this.testFontFamiliesAndCheck(
+            TextStyleDialogComponentFilter.with("Times New Roman"),
+            Lists.of(
+                FontFamily.with("Courier"),
+                FontFamily.with("Times New Roman")
+            ),
+            true
+        );
+    }
+
+    @Test
+    public void testTestFontFamiliesMismatch() {
+        this.testFontFamiliesAndCheck(
+            TextStyleDialogComponentFilter.with("Not"),
+            Lists.of(
+                FontFamily.with("Courier"),
+                FontFamily.with("Times New Roman")
+            ),
+            false
+        );
+    }
+
+    private void testFontFamiliesAndCheck(final TextStyleDialogComponentFilter filter,
+                                          final List<FontFamily> fontFamilies,
+                                          final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.testFontFamilies(fontFamilies),
+            () -> filter + " " + fontFamilies
+        );
+    }
+
+    // testFontFamily...................................................................................................
+
+    @Test
+    public void testTestFontFamilyMatch() {
+        this.testFontFamilyAndCheck(
+            TextStyleDialogComponentFilter.with("Times New Roman"),
+            FontFamily.with("Times New Roman"),
+            true
+        );
+    }
+
+    @Test
+    public void testTestFontFamilyMismatch() {
+        this.testFontFamilyAndCheck(
+            TextStyleDialogComponentFilter.with("Not"),
+            FontFamily.with("Courier"),
+            false
+        );
+    }
+
+    private void testFontFamilyAndCheck(final TextStyleDialogComponentFilter filter,
+                                        final FontFamily fontFamily,
+                                        final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.testFontFamily(fontFamily),
+            () -> filter + " " + fontFamily
+        );
+    }
+
+    // test.............................................................................................................
+
+    @Test
+    public void testTestMatch() {
+        this.testAndCheck(
+            TextStyleDialogComponentFilter.with("Apple"),
+            "APPle",
+            true
+        );
+    }
+
+    @Test
+    public void testTestMismatch() {
+        this.testAndCheck(
+            TextStyleDialogComponentFilter.with("Not"),
+            "Banana",
+            false
+        );
+    }
+
+    private void testAndCheck(final TextStyleDialogComponentFilter filter,
+                              final String text,
+                              final boolean expected) {
+        this.checkEquals(
+            expected,
+            filter.test(text),
+            () -> filter + " " + CharSequences.quoteAndEscape(text)
+        );
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+            TextStyleDialogComponentFilter.with("Filter123"),
+            "Filter123"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<TextStyleDialogComponentFilter> type() {
+        return TextStyleDialogComponentFilter.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
