9b76af3edb06483c8b4ee3869d38a92d04643988
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-05-05 12:24:38 +1000

    TextStyleSampleComponent implements FormValueComponent was ValueComponent
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-dominokit/issues/8165
    - TextStyleSampleComponent implements FormValueComponent

--- src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/sample/TextStyleSampleComponent.java
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/textstyle/sample/TextStyleSampleComponent.java
@@ -18,16 +18,21 @@
 package walkingkooka.spreadsheet.dominokit.value.textstyle.sample;
 
 import elemental2.dom.HTMLDivElement;
+import elemental2.dom.HTMLFieldSetElement;
+import walkingkooka.collect.list.Lists;
 import walkingkooka.spreadsheet.dominokit.HtmlComponent;
 import walkingkooka.spreadsheet.dominokit.HtmlComponentDelegator;
 import walkingkooka.spreadsheet.dominokit.dom.DivComponent;
 import walkingkooka.spreadsheet.dominokit.dom.HtmlElementComponent;
-import walkingkooka.spreadsheet.dominokit.value.ValueComponent;
+import walkingkooka.spreadsheet.dominokit.value.FormElementComponent;
+import walkingkooka.spreadsheet.dominokit.value.FormValueComponent;
 import walkingkooka.spreadsheet.dominokit.value.ValueWatcher;
 import walkingkooka.spreadsheet.dominokit.value.textstyle.TextStyleDialogComponent;
+import walkingkooka.text.CharSequences;
 import walkingkooka.text.printer.IndentingPrinter;
 import walkingkooka.tree.text.TextStyle;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -35,9 +40,9 @@ import java.util.Optional;
  * A {@link walkingkooka.spreadsheet.dominokit.Component} that contains sample text and accepting {@link TextStyle},
  * which is probably copied from {@link TextStyleDialogComponent}.
  */
-final public class TextStyleSampleComponent implements ValueComponent<HTMLDivElement, TextStyle, TextStyleSampleComponent>,
-    HtmlComponentDelegator<HTMLDivElement, TextStyleSampleComponent>,
-    ValueWatcher<TextStyle>{
+final public class TextStyleSampleComponent implements FormValueComponent<HTMLFieldSetElement, TextStyle, TextStyleSampleComponent>,
+    HtmlComponentDelegator<HTMLFieldSetElement, TextStyleSampleComponent>,
+    ValueWatcher<TextStyle> {
 
     public static TextStyleSampleComponent empty() {
         return new TextStyleSampleComponent();
@@ -50,9 +55,11 @@ final public class TextStyleSampleComponent implements ValueComponent<HTMLDivEle
                 "The quick brown fox jumps over the lazy dog"
             );
 
-        this.root = HtmlElementComponent.div()
-            .setCssText("margin-top: 4px; margin-bottom: 4px; margin-left: auto; margin-right: auto; width: 100%;")
-            .appendChild(this.target);
+        this.formElementComponent = FormElementComponent.with(
+            HtmlElementComponent.div()
+                .setCssText("margin-top: 4px; margin-bottom: 4px; margin-left: auto; margin-right: auto; width: 100%;")
+                .appendChild(this.target)
+        );
     }
 
     // ValueWatcher.....................................................................................................
@@ -97,6 +104,76 @@ final public class TextStyleSampleComponent implements ValueComponent<HTMLDivEle
     }
 
     @Override
+    public TextStyleSampleComponent optional() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TextStyleSampleComponent required() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isRequired() {
+        return false;
+    }
+
+    @Override
+    public TextStyleSampleComponent validate() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> errors() {
+        return Lists.empty();
+    }
+
+    @Override
+    public TextStyleSampleComponent setErrors(final List<String> errors) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TextStyleSampleComponent setLabel(final String label) {
+        this.formElementComponent.setLabel(label);
+        return this;
+    }
+
+    @Override
+    public String label() {
+        return this.formElementComponent.getLabel();
+    }
+
+    @Override
+    public Optional<String> helperText() {
+        final String helperText = this.formElementComponent.getHelperText();
+
+        return Optional.ofNullable(
+            CharSequences.isNullOrEmpty(helperText) ?
+                null :
+                helperText
+        );
+    }
+
+    @Override
+    public TextStyleSampleComponent setHelperText(final Optional<String> text) {
+        this.formElementComponent.setHelperText(
+            text.orElse("")
+        );
+        return this;
+    }
+
+    @Override
+    public TextStyleSampleComponent alwaysShowHelperText() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isEditing() {
+        return false;
+    }
+
+    @Override
     public TextStyleSampleComponent setDisabled(final boolean disabled) {
         return this;
     }
@@ -116,10 +193,7 @@ final public class TextStyleSampleComponent implements ValueComponent<HTMLDivEle
         return this;
     }
 
-    @Override
-    public boolean isEditing() {
-        return false;
-    }
+    // TreePrintable....................................................................................................
 
     @Override
     public void printTree(final IndentingPrinter printer) {
@@ -127,7 +201,7 @@ final public class TextStyleSampleComponent implements ValueComponent<HTMLDivEle
 
         printer.indent();
         {
-            this.target.printTree(printer);
+            this.formElementComponent.printTree(printer);
         }
         printer.outdent();
     }
@@ -135,10 +209,10 @@ final public class TextStyleSampleComponent implements ValueComponent<HTMLDivEle
     // HtmlComponentDelegator...........................................................................................
 
     @Override
-    public HtmlComponent<HTMLDivElement, ?> htmlComponent() {
-        return this.root;
+    public HtmlComponent<HTMLFieldSetElement, ?> htmlComponent() {
+        return this.formElementComponent;
     }
 
-    private final DivComponent root;
+    private final FormElementComponent<TextStyle, HTMLDivElement, ?> formElementComponent;
 
 }
