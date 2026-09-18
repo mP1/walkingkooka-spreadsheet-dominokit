ac4c1a5cd5fc94aa64ee1783a4400c639e02d8bd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-18 19:38:26 +1000

    PluginNameAnchorListComponentDelegator

---
+++ src/main/java/walkingkooka/spreadsheet/dominokit/value/plugin/PluginNameAnchorListComponentDelegator.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.dominokit.value.plugin;
+
+import elemental2.dom.HTMLDivElement;
+import walkingkooka.naming.Name;
+import walkingkooka.plugin.PluginAliasLike;
+import walkingkooka.plugin.PluginAliasSetLike;
+import walkingkooka.plugin.PluginInfoLike;
+import walkingkooka.plugin.PluginInfoSetLike;
+import walkingkooka.plugin.PluginSelectorLike;
+import walkingkooka.spreadsheet.dominokit.value.ValueComponentDelegator;
+import walkingkooka.text.printer.IndentingPrinter;
+
+public interface PluginNameAnchorListComponentDelegator<N extends Name & Comparable<N>,
+    I extends PluginInfoLike<I, N>,
+    IS extends PluginInfoSetLike<N, I, IS, S, A, AS>,
+    S extends PluginSelectorLike<N>,
+    A extends PluginAliasLike<N, S, A>,
+    AS extends PluginAliasSetLike<N, I, IS, S, A, AS>> extends ValueComponentDelegator<HTMLDivElement, N, PluginNameAnchorListComponent<N, I, IS, S, A, AS>> {
+
+    // PluginNameAnchorListComponentDelegator...........................................................................
+
+    @Override
+    PluginNameAnchorListComponent<N, I, IS, S, A, AS> valueComponent();
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    default void printTree(final IndentingPrinter printer) {
+        printer.println(this.getClass().getSimpleName());
+        {
+            printer.indent();
+            {
+                this.valueComponent()
+                    .printTree(printer);
+            }
+            printer.outdent();
+        }
+    }
+}
